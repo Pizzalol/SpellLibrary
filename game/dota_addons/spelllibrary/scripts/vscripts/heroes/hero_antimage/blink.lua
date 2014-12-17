3a8b2174408d6f1b8c9e70152ff0cfac5449670d
Pizzalol <siprak214@hotmail.com>
2014-12-17 16:14:05 +0100

    Finished blink. Antimage done

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_antimage/blink.lua
@@ -0,0 +1,20 @@
+--[[Author: Amused/D3luxe
+	Used by: Pizzalol
+	Date: 17.12.2014.
+	Blinks the target to the target point, if the point is beyond max blink range then blink the maximum range]]
+function Blink(keys)
+	--PrintTable(keys)
+	local point = keys.target_points[1]
+	local caster = keys.caster
+	local casterPos = caster:GetAbsOrigin()
+	local pid = caster:GetPlayerID()
+	local difference = point - casterPos
+	local ability = keys.ability
+	local range = ability:GetLevelSpecialValueFor("blink_range", (ability:GetLevel() - 1))
+
+	if difference:Length2D() > range then
+		point = casterPos + (point - casterPos):Normalized() * range
+	end
+
+	FindClearSpaceForUnit(caster, point, false)	
+end
