06d192c50b03334fb853a3722ce503fc2531205e
Pizzalol <siprak214@hotmail.com>
2015-01-03 14:21:23 +0100

    Fixed Bounty Track bug

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_bounty_hunter/track.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_bounty_hunter/track.lua
@@ -10,15 +10,23 @@ function Track( keys )
 	local bonus_gold = ability:GetLevelSpecialValueFor("bonus_gold", (ability:GetLevel() - 1))
 	local bonus_gold_radius = ability:GetLevelSpecialValueFor("bonus_gold_radius", (ability:GetLevel() - 1))
 
+	-- Checks if the target is alive when the modifier is destroyed
 	if not target:IsAlive() then
+
+		-- Gives gold to the caster
+		caster:ModifyGold(bonus_gold_self, true, 0)
+		-- Finds all valid friendly heroes within the bonus gold radius
 		local bonus_gold_targets = FindUnitsInRadius(caster:GetTeam() , targetLocation, nil, bonus_gold_radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY , DOTA_UNIT_TARGET_HERO, 0, 0, false)
 
+		-- Grants them gold but we must exclude the caster
 		for i,v in ipairs(bonus_gold_targets) do
-			v:ModifyGold(bonus_gold, true, 0)
+			if not (v == caster) then
+				v:ModifyGold(bonus_gold, true, 0)
+			end
 		end
-
-		caster:ModifyGold(bonus_gold_self, true, 0)
 	end
 
+	-- Remove the track aura from the target
+	-- NOTE: Trying to do this in KV is not possible it seems
 	target:RemoveModifierByName("modifier_track_aura_datadriven") 
 end
