8c18c3448492c9fa1c4c57a3a41b9f477274d30a
kritth <rkasidis@gmail.com>
2015-01-05 18:54:09 -0500

    Remove dummy dependency
    Change from using dummy to visibility node to reduce cost

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_crystalmaiden/crystal_nova.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_crystalmaiden/crystal_nova.lua
@@ -1,20 +1,15 @@
 --[[
 	Author: kritth
-	Date: 1.1.2015.
+	Date: 5.1.2015.
 	Provide vision post-attack
 ]]
 function crystal_nova_post_vision( keys )
 	-- Variables
-	local caster = keys.caster
-	local target = keys.target
 	local ability = keys.ability
+	local target = keys.target_points[ 1 ]
 	local duration = ability:GetLevelSpecialValueFor( "vision_duration", ( ability:GetLevel() - 1 ) )
-	local modifierName = "modifier_crystal_nova_post_vision_datadriven"
+	local radius = ability:GetLevelSpecialValueFor( "vision_radius", ( ability:GetLevel() - 1 ) )
 
 	-- Create unit to reference the point
-	local dummy = CreateUnitByName( "npc_dummy_blank", keys.target_points[1], false, caster, caster, caster:GetTeamNumber() )
-	ability:ApplyDataDrivenModifier( caster, dummy, modifierName, {} )
-	
-	-- Create timer to destroy
-	Timers:CreateTimer( duration, function() dummy:Destroy() end )
+	ability:CreateVisibilityNode( target, radius, duration )
 end
