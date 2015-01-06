101b19dfdf30de508a67b53541d0849a48d08e5a
Pizzalol <siprak214@hotmail.com>
2015-01-06 15:20:23 +0100

    Finished Necrolyte

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_necrolyte/reapers_scythe.lua
@@ -0,0 +1,27 @@
+--[[Author: Pizzalol
+	Date: 06.01.2015.
+	Deals damage depending on missing hp
+	If the target dies then it increases the respawn time]]
+function ReapersScythe( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+	local target_missing_hp = target:GetMaxHealth() - target:GetHealth()
+	local damage_per_health = ability:GetLevelSpecialValueFor("damage_per_health", (ability:GetLevel() - 1))
+	local respawn_time = ability:GetLevelSpecialValueFor("respawn_constant", (ability:GetLevel() - 1))
+
+	local damage_table = {}
+
+	damage_table.attacker = caster
+	damage_table.victim = target
+	damage_table.ability = ability
+	damage_table.damage_type = ability:GetAbilityDamageType()
+	damage_table.damage = target_missing_hp * damage_per_health
+
+	ApplyDamage(damage_table)
+
+	-- Checking if target is alive to decide if it needs to increase respawn time
+	if not target:IsAlive() then
+		target:SetTimeUntilRespawn(target:GetTimeUntilRespawn() + respawn_time)
+	end
+end
