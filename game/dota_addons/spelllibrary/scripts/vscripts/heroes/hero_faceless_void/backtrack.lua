63fde36b436dc48d3c3325f51a37d7afb3054983
Pizzalol <siprak214@hotmail.com>
2015-01-06 23:15:12 +0100

    Improved Backtrack code

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_faceless_void/backtrack.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_faceless_void/backtrack.lua
@@ -26,34 +26,3 @@ function BacktrackHeal( keys )
 
 	caster:SetHealth(healthTable[caster].old)
 end
-
---[[Author: Pizzalol
-	Date: 06.01.2015.
-	Checks if the attack would have been lethal
-	If yes then it removes the backtrack modifier and applies damage]]
-function BacktrackLethal( keys )
-	local caster = keys.caster
-	local attacker = keys.attacker
-	local incoming_damage = keys.incoming_damage
-	local modifier = keys.modifier_name
-
-	if incoming_damage >= healthTable[caster].old then
-		caster:RemoveModifierByName(modifier)
-
-		local damage_table = {}
-
-		damage_table.attacker = attacker
-		damage_table.victim = caster
-		damage_table.damage_type = DAMAGE_TYPE_PURE
-		damage_table.damage = incoming_damage
-
-		ApplyDamage(damage_table)
-
-		-- For some odd cases where the target would be still alive after taking lethal damage
-		Timers:CreateTimer(0.01, function() 
-			if target:IsAlive() then
-				caster:ApplyDataDrivenModifier(caster, caster, modifier, {})
-			end
-		end)
-	end
-end
