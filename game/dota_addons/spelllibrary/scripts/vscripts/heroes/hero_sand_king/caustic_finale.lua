d71f28023314306e4eb643b56553f80ef84f08b7
kritth <rkasidis@gmail.com>
2015-01-09 16:52:10 -0500

    Remove unnecessary function

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_sand_king/caustic_finale.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_sand_king/caustic_finale.lua
@@ -1,6 +1,11 @@
 --[[
+	CHANGELIST:
+	09.01.2015 - Remove ReleaseParticleIndex( .. )
+]]
+
+--[[
 	Author: kritth
-	Date: 3.1.2015.
+	Date: 09.01.2015
 	Fire effect at target's location upon dead
 ]]
 function caustic_finale_dead_effect( keys )
@@ -10,7 +15,6 @@ function caustic_finale_dead_effect( keys )
 		local soundEventName = "Ability.SandKing_CausticFinale"
 		
 		local fxIndex = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, keys.target )
-		ParticleManager:ReleaseParticleIndex( fxIndex )
 		StartSoundEvent( soundEventName, keys.target )
 	end
 end
