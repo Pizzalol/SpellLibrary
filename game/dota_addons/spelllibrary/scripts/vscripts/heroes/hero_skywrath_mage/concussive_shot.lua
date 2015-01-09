0d303c207d55a507e0e45a6f68f927fec60cdda5
kritth <rkasidis@gmail.com>
2015-01-09 16:48:23 -0500

    Standized variables

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_skywrath_mage/concussive_shot.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_skywrath_mage/concussive_shot.lua
@@ -1,19 +1,28 @@
 --[[
+	CHANGELIST:
+	09.01.2015 - Standized variables
+]]
+
+--[[
 	Author: kritth
-	Date: 8.1.2015.
+	Date: 09.01.2015
 	Find closest hero fire projectile at it
 ]]
 function concussive_shot_seek_target( keys )
+	-- Variables
 	local caster = keys.caster
 	local ability = keys.ability
 	local particle_name = "particles/units/heroes/hero_skywrath_mage/skywrath_mage_concussive_shot.vpcf"
 	local radius = ability:GetLevelSpecialValueFor( "launch_radius", ability:GetLevel() - 1 )
 	local speed = ability:GetLevelSpecialValueFor( "speed", ability:GetLevel() - 1 )
+	local targetTeam = ability:GetAbilityTargetTeam() -- DOTA_UNIT_TARGET_TEAM_ENEMY
+	local targetType = ability:GetAbilityTargetType() -- DOTA_UNIT_TARGET_HERO
+	local targetFlag = ability:GetAbilityTargetFlags() -- DOTA_UNIT_TARGET_FLAG_NO_INVIS + DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS
 	
 	-- pick up x nearest target heroes and create tracking projectile targeting the number of targets
 	local units = FindUnitsInRadius(
-		caster:GetTeamNumber(), caster:GetAbsOrigin(), caster, radius, DOTA_UNIT_TARGET_TEAM_ENEMY,
-		DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NO_INVIS + DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS, FIND_CLOSEST, false
+		caster:GetTeamNumber(), caster:GetAbsOrigin(), caster, radius, targetTeam,
+		targetType, targetFlag, FIND_CLOSEST, false
 	)
 	
 	-- Seek out target
