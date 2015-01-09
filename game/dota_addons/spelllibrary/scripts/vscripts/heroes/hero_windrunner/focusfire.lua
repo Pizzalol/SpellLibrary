9ad278775b66886fdd42718bc1bc537f24377eb0
kritth <rkasidis@gmail.com>
2015-01-09 16:21:55 -0500

    Standized variables.

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_windrunner/focusfire.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_windrunner/focusfire.lua
@@ -1,16 +1,26 @@
 --[[
+	CHANGE LIST:
+	09.01.2015 - Standizard the variables, remove unnecessary funcion
+]]
+
+--[[
 	Author: kritth
 	Date: 5.1.2015.
 	Register the target and order to attack
 ]]
 function focusfire_register( keys )
+	-- Variables
 	local caster = keys.caster
 	local ability = keys.ability
+	local modifierAttackSpeed = "modifier_focusfire_attackspeed_buff_datadriven"
+	local modifierDamageDebuff = "modifier_focusfire_damage_debuff_datadriven"
+	
+	-- Set target
 	caster.focusfire_target = keys.target
 	
 	-- Apply buff
-	ability:ApplyDataDrivenModifier( caster, caster, "modifier_focusfire_attackspeed_buff_datadriven", {} )
-	ability:ApplyDataDrivenModifier( caster, caster, "modifier_focusfire_damage_debuff_datadriven", {} )
+	ability:ApplyDataDrivenModifier( caster, caster, modifierAttackSpeed, {} )
+	ability:ApplyDataDrivenModifier( caster, caster, modifierDamageDebuff, {} )
 	
 	-- Order to attack immediately
 	local order =
@@ -27,29 +37,17 @@ end
 	Date: 5.1.2015.
 	Add/Remove damage debuff modifier when attack start 
 ]]
-function focusfire_on_attack_start( keys )
-	local caster = keys.caster
-	local target = keys.target
-	local ability = keys.ability
-	if target ~= caster.focusfire_target then
-		caster:RemoveModifierByName( "modifier_focusfire_attackspeed_buff_datadriven" )
-	else
-		ability:ApplyDataDrivenModifier( caster, caster, "modifier_focusfire_attackspeed_buff_datadriven", {} )
-	end
-end
-
---[[
-	Author: kritth
-	Date: 5.1.2015.
-	Add/Remove damage debuff modifier when attack has already landed
-]]
 function focusfire_on_attack_landed( keys )
+	-- Variables
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
+	local modifierToRemove = "modifier_focusfire_attackspeed_buff_datadriven"
+	
+	-- Check if it hit the specified target
 	if target ~= caster.focusfire_target then
-		caster:RemoveModifierByName( "modifier_focusfire_damage_debuff_datadriven" )
+		caster:RemoveModifierByName( modifierToRemove )
 	else
-		ability:ApplyDataDrivenModifier( caster, caster, "modifier_focusfire_damage_debuff_datadriven", {} )
+		ability:ApplyDataDrivenModifier( caster, caster, modifierToRemove, {} )
 	end
 end
