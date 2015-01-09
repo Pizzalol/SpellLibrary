1ac90d58b1ea4622ec157a631b3aa835c5274523
MNoya <martinnoya@gmail.com>
2015-01-09 16:50:28 -0200

    Finished Centaur Return

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_centaur/return.lua
@@ -0,0 +1,22 @@
+--[[
+	Author: Noya
+	Date: 9.1.2015.
+	Does base damage plus a percent of the hero strength
+]]
+function Return( event )
+	-- Variables
+	local caster = event.caster
+	local attacker = event.attacker
+	local ability = event.ability
+	local casterSTR = caster:GetStrength()
+	local str_return = ability:GetLevelSpecialValueFor( "strength_pct" , ability:GetLevel() - 1  ) * 0.01
+	local damage = ability:GetLevelSpecialValueFor( "return_damage" , ability:GetLevel() - 1  )
+	local damageType = ability:GetAbilityDamageType()
+	local return_damage = damage + ( casterSTR * str_return )
+
+	-- Damage
+	ApplyDamage({ victim = attacker, attacker = caster, damage = return_damage, damage_type = damageType })
+
+	print("done "..return_damage)
+
+end
