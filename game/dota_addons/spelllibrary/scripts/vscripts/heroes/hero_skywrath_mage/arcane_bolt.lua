6343056c100bd8e1fefe864e9b2fb2916a0b8931
kritth <rkasidis@gmail.com>
2015-01-09 16:46:01 -0500

    Standized variables

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_skywrath_mage/arcane_bolt.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_skywrath_mage/arcane_bolt.lua
@@ -1,4 +1,9 @@
 --[[
+	CHANGELIST:
+	09.01.2015 - Standized variables
+]]
+
+--[[
 	Author: kritth
 	Date: 8.1.2015.
 	Initialize linked list for multiple instances of damage
@@ -19,10 +24,11 @@ end
 
 --[[
 	Author: kritth
-	Date: 8.1.2015.
+	Date: 09.01.2015
 	Calculating the damage for arcane bolt
 ]]
 function arcane_bolt_hit( keys )
+	-- Variables
 	local caster = keys.caster
 	local target = keys.target
 	local ability = keys.ability
@@ -31,6 +37,7 @@ function arcane_bolt_hit( keys )
 	local base_damage = ability:GetLevelSpecialValueFor( "bolt_damage", ability:GetLevel() - 1 )
 	local multiplier = ability:GetLevelSpecialValueFor( "int_multiplier", ability:GetLevel() - 1 )
 	local intelligence = math.max( 0, caster.arcane_bolt_list_head.value )
+	local damageType = ability:GetAbilityDamageType() -- DAMAGE_TYPE_MAGICAL
 	
 	-- Update linked head node
 	if caster.arcane_bolt_list_head ~= caster.arcane_bolt_list_tail then
@@ -44,7 +51,7 @@ function arcane_bolt_hit( keys )
 		victim = target,
 		attacker = caster,
 		damage = base_damage + intelligence * multiplier,
-		damage_type = DAMAGE_TYPE_MAGICAL,
+		damage_type = damageType,
 	}
 	ApplyDamage( damageTable )
 	
