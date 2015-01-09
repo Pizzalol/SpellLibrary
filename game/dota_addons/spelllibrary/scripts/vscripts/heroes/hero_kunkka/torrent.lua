eb073fca98d026a690cfdc5ff6e2e86d7e51d403
kritth <rkasidis@gmail.com>
2015-01-09 07:35:44 -0500

    Finished Kunkka's torrent ability

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_kunkka/torrent.lua
@@ -0,0 +1,54 @@
+--[[
+	Author: kritth
+	Date: 9.1.2015.
+	Bubbles seen only to ally as pre-effect
+]]
+function torrent_bubble_allies( keys )
+	local caster = keys.caster
+	
+	local allHeroes = HeroList:GetAllHeroes()
+	local delay = keys.ability:GetLevelSpecialValueFor( "delay", keys.ability:GetLevel() - 1 )
+	local particleName = "particles/units/heroes/hero_kunkka/kunkka_spell_torrent_bubbles.vpcf"
+	local target = keys.target_points[1]
+	
+	for k, v in pairs( allHeroes ) do
+		if v:GetPlayerID() and v:GetTeam() == caster:GetTeam() then
+			local fxIndex = ParticleManager:CreateParticleForPlayer( particleName, PATTACH_ABSORIGIN, v, PlayerResource:GetPlayer( v:GetPlayerID() ) )
+			ParticleManager:SetParticleControl( fxIndex, 0, target )
+			
+			EmitSoundOnClient( "Ability.pre.Torrent", PlayerResource:GetPlayer( v:GetPlayerID() ) )
+			
+			-- Destroy particle after delay
+			Timers:CreateTimer( delay, function()
+					ParticleManager:DestroyParticle( fxIndex, false )
+					return nil
+				end
+			)
+		end
+	end
+end
+
+--[[
+	Author: kritth
+	Date: 9.1.2015.
+	Emit sound at location
+]]
+function torrent_emit_sound( keys )
+	local dummy = CreateUnitByName( "npc_dummy_blank", keys.target_points[1], false, keys.caster, keys.caster, keys.caster:GetTeamNumber() )
+	EmitSoundOn( "Ability.Torrent", dummy )
+	dummy:ForceKill( true )
+end
+
+--[[
+	Author: kritth
+	Date: 9.1.2015.
+	Provide post vision
+]]
+function torrent_post_vision( keys )
+	local ability = keys.ability
+	local target = keys.target_points[1]
+	local radius = ability:GetLevelSpecialValueFor( "radius", ability:GetLevel() - 1 )
+	local duration = ability:GetLevelSpecialValueFor( "vision_duration", ability:GetLevel() - 1 )
+	
+	ability:CreateVisibilityNode( target, radius, duration )
+end
