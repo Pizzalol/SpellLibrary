dd0256046f6becfa417df1298af33cd9c9d53a26
kritth <rkasidis@gmail.com>
2015-01-08 20:14:59 -0500

    Finished skywrath mage's concussive shot.

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_skywrath_mage/concussive_shot.lua
@@ -0,0 +1,52 @@
+--[[
+	Author: kritth
+	Date: 8.1.2015.
+	Find closest hero fire projectile at it
+]]
+function concussive_shot_seek_target( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local particle_name = "particles/units/heroes/hero_skywrath_mage/skywrath_mage_concussive_shot.vpcf"
+	local radius = ability:GetLevelSpecialValueFor( "launch_radius", ability:GetLevel() - 1 )
+	local speed = ability:GetLevelSpecialValueFor( "speed", ability:GetLevel() - 1 )
+	
+	-- pick up x nearest target heroes and create tracking projectile targeting the number of targets
+	local units = FindUnitsInRadius(
+		caster:GetTeamNumber(), caster:GetAbsOrigin(), caster, radius, DOTA_UNIT_TARGET_TEAM_ENEMY,
+		DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NO_INVIS + DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS, FIND_CLOSEST, false
+	)
+	
+	-- Seek out target
+	for k, v in pairs( units ) do
+		local projTable = {
+			EffectName = particle_name,
+			Ability = ability,
+			Target = v,
+			Source = caster,
+			bDodgeable = true,
+			bProvidesVision = true,
+			vSpawnOrigin = caster:GetAbsOrigin(),
+			iMoveSpeed = speed,
+			iVisionRadius = radius,
+			iVisionTeamNumber = caster:GetTeamNumber(),
+			iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
+		}
+		ProjectileManager:CreateTrackingProjectile( projTable )
+		break
+	end
+end
+
+--[[
+	Author: kritth
+	Date: 8.1.2015.
+	Give post attack vision
+]]
+function concussive_shot_post_vision( keys )
+	local target = keys.target:GetAbsOrigin()
+	local ability = keys.ability
+	local radius = ability:GetLevelSpecialValueFor( "launch_radius", ability:GetLevel() - 1 )
+	local duration = ability:GetLevelSpecialValueFor( "vision_duration", ability:GetLevel() - 1 )
+
+	-- Create node
+	ability:CreateVisibilityNode( target, radius, duration )
+end
