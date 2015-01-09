f763673ad199918a38f619ab04bd9a4a28191639
kritth <rkasidis@gmail.com>
2015-01-08 20:03:39 -0500

    Finished skywrath mage's arcane bolt.

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_skywrath_mage/arcane_bolt.lua
@@ -0,0 +1,53 @@
+--[[
+	Author: kritth
+	Date: 8.1.2015.
+	Initialize linked list for multiple instances of damage
+]]
+function arcane_bolt_init( keys )
+	local caster = keys.caster
+	local intelligence = keys.caster:GetIntellect()
+	if caster.arcane_bolt_list_head == nil then
+		caster.arcane_bolt_list_tail = {}
+		caster.arcane_bolt_list_head = { next = caster.arcane_bolt_list_tail, value = intelligence }
+	else
+		local tmp = {}
+		caster.arcane_bolt_list_tail.next = tmp
+		caster.arcane_bolt_list_tail.value = intelligence
+		caster.arcane_bolt_list_tail = tmp
+	end
+end
+
+--[[
+	Author: kritth
+	Date: 8.1.2015.
+	Calculating the damage for arcane bolt
+]]
+function arcane_bolt_hit( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+	local radius = ability:GetLevelSpecialValueFor( "bolt_vision", ability:GetLevel() - 1 )
+	local duration = ability:GetLevelSpecialValueFor( "vision_duration", ability:GetLevel() - 1 )
+	local base_damage = ability:GetLevelSpecialValueFor( "bolt_damage", ability:GetLevel() - 1 )
+	local multiplier = ability:GetLevelSpecialValueFor( "int_multiplier", ability:GetLevel() - 1 )
+	local intelligence = math.max( 0, caster.arcane_bolt_list_head.value )
+	
+	-- Update linked head node
+	if caster.arcane_bolt_list_head ~= caster.arcane_bolt_list_tail then
+		caster.arcane_bolt_list_head = caster.arcane_bolt_list_head.next
+	else
+		caster.arcane_bolt_list_head = nil
+	end
+	
+	-- Deal damage
+	local damageTable = {
+		victim = target,
+		attacker = caster,
+		damage = base_damage + intelligence * multiplier,
+		damage_type = DAMAGE_TYPE_MAGICAL,
+	}
+	ApplyDamage( damageTable )
+	
+	-- Provide visibility
+	ability:CreateVisibilityNode( target:GetAbsOrigin(), radius, duration )
+end
