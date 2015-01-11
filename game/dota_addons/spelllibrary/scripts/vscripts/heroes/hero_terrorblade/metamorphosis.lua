096eb735bfb175216a9b1546d22bfdab748cc6b1
MNoya <martinnoya@gmail.com>
2015-01-11 01:11:59 -0200

    Finished Terrorblade Metamorphosis
    Epic Hide and ShowWearable method, which is probably ready to be used
    for many other spells
    
    I didn't bother with the transform_time.

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_terrorblade/metamorphosis.lua
@@ -0,0 +1,87 @@
+--[[Author: Pizzalol/Noya
+	Date: 10.01.2015.
+	Swaps the ranged attack, projectile and caster model
+]]
+function ModelSwapStart( keys )
+	local caster = keys.caster
+	local model = keys.model
+	local projectile_model = keys.projectile_model
+
+	-- Saves the original model and attack capability
+	if caster.caster_model == nil then 
+		caster.caster_model = caster:GetModelName()
+	end
+	caster.caster_attack = caster:GetAttackCapability()
+
+	-- Sets the new model and projectile
+	caster:SetOriginalModel(model)
+	caster:SetRangedProjectileName(projectile_model)
+
+	-- Sets the new attack type
+	caster:SetAttackCapability(DOTA_UNIT_CAP_RANGED_ATTACK)
+end
+
+--[[Author: Pizzalol/Noya
+	Date: 10.01.2015.
+	Reverts back to the original model and attack type
+]]
+function ModelSwapEnd( keys )
+	local caster = keys.caster
+
+	caster:SetModel(caster.caster_model)
+	caster:SetOriginalModel(caster.caster_model)
+	caster:SetAttackCapability(caster.caster_attack)
+end
+
+--[[Author: Noya
+	Date: 10.01.2015.
+	Hides all dem hats
+]]
+function HideWearables( event )
+	local hero = event.caster
+	local ability = event.ability
+	local duration = ability:GetLevelSpecialValueFor( "duration", ability:GetLevel() - 1 )
+	print("Hiding Wearables")
+	--hero:AddNoDraw() -- Doesn't work on classname dota_item_wearable
+
+	hero.wearableNames = {} -- In here we'll store the wearable names to revert the change
+	hero.hiddenWearables = {} -- Keep every wearable handle in a table, as its way better to iterate than in the MovePeer system
+    local model = hero:FirstMoveChild()
+    while model ~= nil do
+        if model:GetClassname() ~= "" and model:GetClassname() == "dota_item_wearable" then
+            local modelName = model:GetModelName()
+            if string.find(modelName, "invisiblebox") == nil then
+            	-- Add the original model name to revert later
+            	table.insert(hero.wearableNames,modelName)
+            	print("Hidden "..modelName.."")
+
+            	-- Set model invisible
+            	model:SetModel("models/development/invisiblebox.vmdl")
+            	table.insert(hero.hiddenWearables,model)
+            end
+        end
+        model = model:NextMovePeer()
+        if model ~= nil then
+        	print("Next Peer:" .. model:GetModelName())
+        end
+    end
+end
+
+--[[Author: Noya
+	Date: 10.01.2015.
+	Shows the hidden hero wearables
+]]
+function ShowWearables( event )
+	local hero = event.caster
+	print("Showing Wearables on ".. hero:GetModelName())
+
+	-- Iterate on both tables to set each item back to their original modelName
+	for i,v in ipairs(hero.hiddenWearables) do
+		for index,modelName in ipairs(hero.wearableNames) do
+			if i==index then
+				print("Changed "..v:GetModelName().. " back to "..modelName)
+				v:SetModel(modelName)
+			end
+		end
+	end
+end
