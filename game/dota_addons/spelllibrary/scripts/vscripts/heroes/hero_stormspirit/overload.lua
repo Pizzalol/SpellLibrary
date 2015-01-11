fd6835ddb7e793b46693d954655585d64d7d4978
kritth <rkasidis@gmail.com>
2015-01-11 14:41:59 -0500

    Finished Stormspirit's Overload

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_stormspirit/overload.lua
@@ -0,0 +1,29 @@
+--[[
+	Author: kritth
+	Date: 11.01.2015.
+	Create a listener to listen to the abiltiy casted in the game
+]]
+function overload_check_order( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	ListenToGameEvent( "dota_player_used_ability", function( event )
+			local ply = EntIndexToHScript( event.PlayerID )
+			-- Check if player existed
+			if ply then
+				local hero = ply:GetAssignedHero()
+				-- Check if it is corrent hero
+				if hero == caster then
+					-- Check if ability on cast bar is casted
+					local ability_count = caster:GetAbilityCount()
+					for i = 1, ability_count do
+						local ability_at_slot = caster:GetAbilityByIndex( i )
+						if ability_at_slot:GetAbilityName() == event.abilityname then
+							ability:ApplyDataDrivenModifier( caster, caster, "modifier_overload_damage_datadriven", {} )
+							break
+						end
+					end
+				end
+			end
+		end, nil
+	)
+end
