7de64cba2b82ab8a8d43ee61b6c08b81b6ab7800
MNoya <martinnoya@gmail.com>
2015-01-11 19:22:45 -0200

    Fixed Sunder Particle
    Note the usage of SetParticleControlEnt for later

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_terrorblade/sunder.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_terrorblade/sunder.lua
@@ -26,4 +26,18 @@ function Sunder( event )
 		target:SetHealth(target_maxHealth * casterHP_percent)
 	end
 
+	-- Show the particle caster-> target
+	local particleName = "particles/units/heroes/hero_terrorblade/terrorblade_sunder.vpcf"	
+	local particle = ParticleManager:CreateParticle( particleName, PATTACH_POINT_FOLLOW, target )
+
+	ParticleManager:SetParticleControlEnt(particle, 0, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
+	ParticleManager:SetParticleControlEnt(particle, 1, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
+
+	-- Show the particle target-> caster
+	local particleName = "particles/units/heroes/hero_terrorblade/terrorblade_sunder.vpcf"	
+	local particle = ParticleManager:CreateParticle( particleName, PATTACH_POINT_FOLLOW, caster )
+
+	ParticleManager:SetParticleControlEnt(particle, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
+	ParticleManager:SetParticleControlEnt(particle, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
+
 end
