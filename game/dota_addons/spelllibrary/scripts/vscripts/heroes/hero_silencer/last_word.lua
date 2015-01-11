78d689f9c32f309be5507a353fe875ce694ba1fb
chrislotix <chrislotix@gmail.com>
2015-01-11 01:16:59 +0100

    Finished Last Word

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_silencer/last_word.lua
@@ -0,0 +1,14 @@
+--[[Last Word stop loop sound
+	Author: chrislotix
+	Date: 11.1.2015.]]
+
+function LastWordStopSound( keys )
+
+	local sound_name = "Hero_Silencer.LastWord.Target"
+	local unit = keys.unit
+
+	StopSoundEvent(sound_name, unit)
+
+	print("test")
+
+end
