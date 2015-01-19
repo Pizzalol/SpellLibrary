7c2a76d5b4b326ab13a43d5d50259b47cc903cd0
Pizzalol <siprak214@hotmail.com>
2015-01-19 22:37:46 +0100

    Finished KotL Recall

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_keeper_of_the_light/recall.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_keeper_of_the_light/recall.lua
@@ -1,22 +1,49 @@
---[[
-	Author: igo95862, Noya
-	Date: 14.1.2015.
-	Disallows self targeting by checking if the target is not the caster when the ability starts
-]]
-function RecallPrecast( event )
-	local caster = event.caster
-	local target = event.target
-	local player = caster:GetPlayerOwner()
-	local pID = caster:GetPlayerOwnerID()
+--[[Author: Pizzalol
+	Date: 19.01.2015.
+	Finds the closest unit to the selected point and then applies the Recall modifier]]
+function Recall( keys )
+	-- Variables
+	local caster = keys.caster
+	local ability = keys.ability
+	local point = keys.target_points[1]
 
-	-- This prevents the spell from going off
-	if target == caster then
-		caster:Stop()
+	-- Extra variables
+	local sound_caster = keys.sound_caster
+	local sound_target = keys.sound_target
+	local modifier = keys.modifier
+	local duration = ability:GetLevelSpecialValueFor("teleport_delay", (ability:GetLevel() - 1))
 
-		-- Play Error Sound
-		EmitSoundOnClient("General.CastFail_InvalidTarget_Hero", player)
+	-- Find the closest target
+	local targets = FindUnitsInRadius(caster:GetTeam(), point, nil, FIND_UNITS_EVERYWHERE, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), ability:GetAbilityTargetFlags(), FIND_CLOSEST, false)
 
-		-- This makes use of the Custom Error Flash module by zedor. https://github.com/zedor/CustomError
-		FireGameEvent( 'custom_error_show', { player_ID = pID, _error = "Ability Can't Target Self" } )
+	-- Apply the modifier to the closest target
+	for _,v in ipairs(targets) do
+		if v ~= caster then
+			ability:ApplyDataDrivenModifier(caster, v, modifier, {duration = duration})
+			EmitSoundOn(sound_target, v)
+
+			-- Stop the sound after the duration
+			Timers:CreateTimer(duration, function()				
+				StopSoundOn(sound_caster, caster)
+				StopSoundOn(sound_target, v)
+				-- Stop the active command of the target upon being teleported
+				v:Stop()
+			end)
+			return
+		end
 	end
+end
+
+--[[Author: Pizzalol
+	Date: 19.01.2015.
+	Finds the closest unit to the selected point and then applies the Recall modifier]]
+function RecallFail( keys )
+	local caster = keys.caster
+	local target = keys.unit
+
+	local sound_caster = keys.sound_caster
+	local sound_target = keys.sound_target
+
+	StopSoundOn(sound_caster, caster)
+	StopSoundOn(sound_target, target)
 end
