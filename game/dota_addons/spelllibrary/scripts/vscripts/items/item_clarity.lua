0aa78c2ba022c8ec6274f59b810887786ef6ce1d
Rookdota <pie4all88@gmail.com>
2015-01-25 13:28:26 -0600

    Added Ghost Scepter, Clarity, and Healing Salve.

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_clarity.lua
@@ -0,0 +1,25 @@
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called when Clarity is cast.  Reduces the charges on the item by one.
+================================================================================================================= ]]
+function item_clarity_on_spell_start(keys)
+	--Reduce the charges left on the item by 1.  Remove the item if there are no charges left.
+	local new_charges = keys.ability:GetCurrentCharges() - 1
+	if new_charges <= 0 then
+		keys.caster:RemoveItem(keys.ability)
+	else  --new_charges > 0 
+		keys.ability:SetCurrentCharges(new_charges)
+	end
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called regularly while Clarity's effect is on a unit.  Restores their mana.
+	Additional parameters: keys.RegenInterval, keys.BuffDuration, keys.TotalManaRegen
+================================================================================================================= ]]
+function item_clarity_modifier_active_on_interval_think(keys)
+	keys.target:GiveMana(keys.TotalManaRegen / (keys.BuffDuration / keys.RegenInterval))
+end
---
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_flask.lua
@@ -0,0 +1,25 @@
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called when Healing Salve is cast.  Reduces the charges on the item by one.
+================================================================================================================= ]]
+function item_flask_on_spell_start(keys)
+	--Reduce the charges left on the item by 1.  Remove the item if there are no charges left.
+	local new_charges = keys.ability:GetCurrentCharges() - 1
+	if new_charges <= 0 then
+		keys.caster:RemoveItem(keys.ability)
+	else  --new_charges > 0 
+		keys.ability:SetCurrentCharges(new_charges)
+	end
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called regularly while Healing Salve's effect is on a unit.  Restores their health.
+	Additional parameters: keys.RegenInterval, keys.BuffDuration, keys.TotalHealthRegen
+================================================================================================================= ]]
+function item_flask_modifier_active_on_interval_think(keys)
+	keys.target:Heal(keys.TotalHealthRegen / (keys.BuffDuration / keys.RegenInterval), keys.caster)
+end
