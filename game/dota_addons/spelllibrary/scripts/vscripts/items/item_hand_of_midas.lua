cb5823a60d5bfcaca8cffadeb7567c643a01e7af
Rookdota <pie4all88@gmail.com>
2015-01-25 15:54:20 -0600

    Added several more items.
    Added Bracer, Hand of Midas, Null Talisman, Oblivion Staff,
    Perseverance, Power Treads, and Wraith Band.

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_hand_of_midas.lua
@@ -0,0 +1,21 @@
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called when Hand of Midas is cast.  Kills the target and awards XP and gold.
+	Additional parameters: keys.BonusGold and keys.XPMultiplier
+================================================================================================================= ]]
+function item_hand_of_midas_on_spell_start(keys)
+	keys.caster:ModifyGold(keys.BonusGold, true, 0)  --Give the player a flat amount of reliable gold.
+	keys.caster:AddExperience(keys.target:GetDeathXP() * keys.XPMultiplier, false, false)  --Give the player some XP.
+	
+	--Start the particle and sound.
+	keys.target:EmitSound("DOTA_Item.Hand_Of_Midas")
+	local midas_particle = ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN_FOLLOW, keys.target)	
+	ParticleManager:SetParticleControlEnt(midas_particle, 1, keys.caster, PATTACH_POINT_FOLLOW, "attach_hitloc", keys.caster:GetAbsOrigin(), false)
+
+	--Remove default gold/XP on the creep before killing it so the caster does not receive anything more.
+	keys.target:SetDeathXP(0)
+	keys.target:SetMinimumGoldBounty(0)
+	keys.target:SetMaximumGoldBounty(0)
+	keys.target:Kill(keys.ability, keys.caster) --Kill the creep.  This increments the caster's last hit counter.
+end
---
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_power_treads.lua
@@ -0,0 +1,52 @@
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	A helper method that switches the keys.ability item to one with the inputted name.
+================================================================================================================= ]]
+function swap_to_item(keys, ItemName)
+	while keys.caster:HasAnyAvailableInventorySpace() do  --Fill all empty slots in the player's inventory with "dummy" items.
+		keys.caster:AddItem(CreateItem("item_dummy_datadriven", keys.caster, keys.caster))
+	end
+	
+	keys.caster:RemoveItem(keys.ability)
+	keys.caster:AddItem(CreateItem(ItemName, keys.caster, keys.caster))  --This should be put into the same slot that the removed item was in.
+	
+	for i=0, 5, 1 do  --Remove all dummy items from the player's inventory.
+		local current_item = keys.caster:GetItemInSlot(i)
+		if current_item ~= nil then
+			if current_item:GetName() == "item_dummy_datadriven" then
+				keys.caster:RemoveItem(current_item)
+			end
+		end
+	end
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called when Power Treads (Strength) is cast.  Swaps the item to Power Treads (Intelligence).
+================================================================================================================= ]]
+function item_power_treads_strength_on_spell_start(keys)
+	swap_to_item(keys, "item_power_treads_intelligence_datadriven")
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called when Power Treads (Agility) is cast.  Swaps the item to Power Treads (Strength).
+================================================================================================================= ]]
+function item_power_treads_agility_on_spell_start(keys)
+	swap_to_item(keys, "item_power_treads_strength_datadriven")
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 25, 2015
+	Called when Power Treads (Intelligence) is cast.  Swaps the item to Power Treads (Agility).
+================================================================================================================= ]]
+function item_power_treads_intelligence_on_spell_start(keys)
+	swap_to_item(keys, "item_power_treads_agility_datadriven")
+end
