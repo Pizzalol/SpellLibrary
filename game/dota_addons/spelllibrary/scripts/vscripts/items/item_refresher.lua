b1de7cabd18df25c62b8e2bf57c6801d757eafac
Rookdota <pie4all88@gmail.com>
2015-01-26 14:49:51 -0600

    Added Refresher Orb and Aghanim's Scepter.

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_refresher.lua
@@ -0,0 +1,24 @@
+--[[ ============================================================================================================
+	Author: Rook
+	Date: January 26, 2015
+	Called when Refresher Orb Rook is cast.  Takes the player's abilities and items off cooldown.
+================================================================================================================= ]]
+function item_refresher_datadriven_on_spell_start(keys)
+	--Refresh all abilities on the caster.
+	for i=0, 15, 1 do  --The maximum number of abilities a unit can have is currently 16.
+		local current_ability = keys.caster:GetAbilityByIndex(i)
+		if current_ability ~= nil then
+			current_ability:EndCooldown()
+		end
+	end
+	
+	--Refresh all items the caster has.
+	for i=0, 5, 1 do
+		local current_item = keys.caster:GetItemInSlot(i)
+		if current_item ~= nil then
+			if current_item:GetName() ~= "item_refresher_datadriven" then  --Refresher Orb does not refresh itself.
+				current_item:EndCooldown()
+			end
+		end
+	end
+end
