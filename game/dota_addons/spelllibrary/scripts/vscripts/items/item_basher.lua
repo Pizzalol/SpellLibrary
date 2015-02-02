ae1eab888d85fa04b5b74836e7248e195262e6dd
Rookdota <pie4all88@gmail.com>
2015-02-01 21:37:57 -0600

    Added Skull Basher and Abyssal Blade.  Refactored Butterfly and added PRD to MKB.

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_basher.lua
@@ -0,0 +1,64 @@
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 1, 2015
+	Called when a unit with Skull Basher lands an attack.  Calculates whether a stun should occur, and applies one
+	if so.
+	Additional parameters: keys.BashChanceMelee and keys.BashChanceRanged
+================================================================================================================= ]]
+function modifier_item_basher_datadriven_bash_chance_on_attack_landed(keys)
+	if not keys.caster:HasModifier("bash_cooldown_modifier") then
+		local random_int = RandomInt(1, 100)
+		local is_ranged_attacker = keys.caster:IsRangedAttacker()
+		
+		if (is_ranged_attacker and random_int <= keys.BashChanceRanged) or (not is_ranged_attacker and random_int <= keys.BashChanceMelee) then
+			keys.target:EmitSound("DOTA_Item.SkullBasher")
+			keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, "modifier_item_basher_datadriven_bash", nil)
+			keys.ability:StartCooldown(keys.ability:GetCooldown(keys.ability:GetLevel()))  --This is just for visual purposes.
+			
+			--Give the caster a generic "bash cooldown" modifier so they cannot bash in the next couple of seconds due to any item.
+			keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "bash_cooldown_modifier", nil)
+		end
+	end
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 1, 2015
+	Called when a bash chance modifier is created or destroyed on the unit.  Ensures that only one of these modifiers
+	is active on the unit, since they should not stack.
+================================================================================================================= ]]
+function modifier_item_basher_datadriven_recalculate_bash_chance(keys)
+	Timers:CreateTimer({
+		callback = function()
+			--Temporarily remove all Skull Basher and Abyssal Blade bash chance modifiers.
+			while keys.caster:HasModifier("modifier_item_basher_datadriven_bash_chance") do
+				keys.caster:RemoveModifierByName("modifier_item_basher_datadriven_bash_chance")
+			end
+			while keys.caster:HasModifier("modifier_item_abyssal_blade_datadriven_bash_chance") do
+				keys.caster:RemoveModifierByName("modifier_item_abyssal_blade_datadriven_bash_chance")
+			end
+
+			--Find out if there is a Skull Basher or Abyssal Blade in the player's inventory.
+			local skull_basher = nil
+			local abyssal_blade = nil
+			for i=0, 5, 1 do
+				local current_item = keys.caster:GetItemInSlot(i)
+				if current_item ~= nil then
+					local item_name = current_item:GetName()
+					if item_name == "item_basher_datadriven" then
+						skull_basher = current_item
+					elseif item_name == "item_abyssal_blade_datadriven" then
+						abyssal_blade = current_item
+					end
+				end
+			end
+			
+			if abyssal_blade ~= nil then  --Prioritize the Abyssal Blade bash chance modifier.
+				abyssal_blade:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_abyssal_blade_datadriven_bash_chance", {duration = -1})
+			elseif skull_basher ~= nil then
+				skull_basher:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_basher_datadriven_bash_chance", {duration = -1})
+			end
+		end
+	})
+end
--- game/dota_addons/spelllibrary/scripts/vscripts/items/item_butterfly.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_butterfly.lua
@@ -18,25 +18,47 @@ end
 --[[ ============================================================================================================
 	Author: Rook
 	Date: February 1, 2015
-	Called when the movespeed modifier expires, or when a Butterfly is acquired	or dropped.  Ensures that the caster
-	has one stack of evasion for every Butterfly in their inventory (so long as they have not been recently casted).
+	Called when the movespeed active modifier expires.	Gives the caster has a stack of evasion for every 
+	Butterfly in their inventory.
 ================================================================================================================= ]]
-function item_butterfly_datadriven_recalculate_evasion(keys)
+function modifier_item_butterfly_datadriven_movespeed_on_destroy(keys)
 	if not keys.caster:HasModifier("modifier_item_butterfly_datadriven_movespeed") then
-		Timers:CreateTimer({callback = function()
-			--Reset all evasion granted by Butterflies in the caster's inventory before adding it back, just to be sure we end up with the right amount.
-			while keys.caster:HasModifier("modifier_item_butterfly_datadriven_evasion") do
-				keys.caster:RemoveModifierByName("modifier_item_butterfly_datadriven_evasion")
-			end
+		--Reset all evasion granted by Butterflies in the caster's inventory before adding it back, just to be sure we end up with the right amount.
+		while keys.caster:HasModifier("modifier_item_butterfly_datadriven_evasion") do
+			keys.caster:RemoveModifierByName("modifier_item_butterfly_datadriven_evasion")
+		end
 
-			for i=0, 5, 1 do
-				local current_item = keys.caster:GetItemInSlot(i)
-				if current_item ~= nil then
-					if current_item:GetName() == "item_butterfly_datadriven" then
-						keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_butterfly_datadriven_evasion", {duration = -1})
-					end
+		for i=0, 5, 1 do
+			local current_item = keys.caster:GetItemInSlot(i)
+			if current_item ~= nil then
+				if current_item:GetName() == "item_butterfly_datadriven" then
+					keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_butterfly_datadriven_evasion", {duration = -1})
 				end
 			end
-		end })
+		end
+	end
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 1, 2015
+	Called when a Butterfly is acquired.  Adds an additional evasion modifier if Butterfly hasn't been cast recently.
+================================================================================================================= ]]
+function modifier_item_butterfly_datadriven_on_created(keys)
+	if not keys.caster:HasModifier("modifier_item_butterfly_datadriven_movespeed") then
+		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_butterfly_datadriven_evasion", {duration = -1})
+	end
+end
+
+
+--[[ ============================================================================================================
+	Author: Rook
+	Date: February 1, 2015
+	Called when a Butterfly is dropped, sold, etc..  Remove an evasion modifier if Butterfly hasn't been cast recently.
+================================================================================================================= ]]
+function modifier_item_butterfly_datadriven_on_destroy(keys)
+	if not keys.caster:HasModifier("modifier_item_butterfly_datadriven_movespeed") then
+		keys.caster:RemoveModifierByName("modifier_item_butterfly_datadriven_evasion")
 	end
 end
