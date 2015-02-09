9a7cc04c995058293f91c6e8cc7c34295494852f
Pizzalol <siprak214@hotmail.com>
2015-02-09 17:53:14 +0100

    Finished Berserkers Call

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_axe/berserkers_call.lua
@@ -0,0 +1,35 @@
+--[[Author: Pizzalol
+	Date: 09.02.2015.
+	Forces the target to attack the caster]]
+function BerserkersCall( keys )
+	local caster = keys.caster
+	local target = keys.target
+
+	-- Clear the force attack target
+	target:SetForceAttackTarget(nil)
+
+	-- Give the attack order if the caster is alive
+	-- otherwise forces the target to sit and do nothing
+	if caster:IsAlive() then
+		local order = 
+		{
+			UnitIndex = target:entindex(),
+			OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
+			TargetIndex = caster:entindex()
+		}
+
+		ExecuteOrderFromTable(order)
+	else
+		target:Stop()
+	end
+
+	-- Set the force attack target to be the caster
+	target:SetForceAttackTarget(caster)
+end
+
+-- Clears the force attack target upon expiration
+function BerserkersCallEnd( keys )
+	local target = keys.target
+
+	target:SetForceAttackTarget(nil)
+end
