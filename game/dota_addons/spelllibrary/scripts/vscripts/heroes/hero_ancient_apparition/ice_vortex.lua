bb5e4ac8cf52efd270f0ae40ae569b6a2dee6348
Pizzalol <siprak214@hotmail.com>
2015-02-11 23:48:47 +0100

    Finished Ice Vortex

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_ancient_apparition/ice_vortex.lua
@@ -0,0 +1,24 @@
+--[[Author: Pizzalol
+	Date: 11.02.2015.
+	Provides vision over the targeted area]]
+function IceVortexVision( keys )
+	local caster = keys.caster
+	local target_point = keys.target_points[1]
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+
+	local duration = ability:GetLevelSpecialValueFor("duration", ability_level)
+	local vision_aoe = ability:GetLevelSpecialValueFor("vision_aoe", ability_level)
+
+	ability:CreateVisibilityNode(target_point, vision_aoe, duration)
+end
+
+--[[Author: Pizzalol
+	Date: 11.02.2015.
+	Stops the sound from playing on the targeted unit]]
+function IceVortexStopSound( keys )
+	local target = keys.target
+	local sound = keys.sound
+
+	StopSoundEvent(sound, target)
+end
