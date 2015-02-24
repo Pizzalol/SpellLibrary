6e0697359cc8a8618a7612513dfb83ea59801d9a
Pizzalol <siprak214@hotmail.com>
2015-02-24 23:14:29 +0100

    Finished Scorched Earth functionality, particle is missing

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_doom_bringer/scorched_earth.lua
@@ -0,0 +1,14 @@
+--[[Author: Pizzalol
+	Date: 24.02.2015.
+	Checks if the target is owned by the caster or if its the caster itself
+	If yes then it applies the scorched earth buff]]
+function ScorchedEarthCheck( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+	local modifier = keys.modifier
+
+	if target:GetOwner() == caster or target == caster then
+		ability:ApplyDataDrivenModifier(caster, target, modifier, {})
+	end
+end
