967794179b4f197d13a7a1126a910f9a802d0349
Pizzalol <siprak214@hotmail.com>
2015-03-16 08:22:20 +0100

    Fixed a bug with Drow Trueshot ability

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_drow/trueshot.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_drow/trueshot.lua
@@ -3,6 +3,9 @@
 	Date: 3.1.2015.
 	Create timer to provide bonus damage
 ]]
+
+--[[Changelog:
+	16.03.2015. Fixed an issue where the timer wouldnt die correctly -- Pizzalol]]
 function trueshot_initialize( keys )
 	local caster = keys.caster
 	local target = keys.target
@@ -12,51 +15,55 @@ function trueshot_initialize( keys )
 	Timers:CreateTimer( DoUniqueString( "trueshot_updateDamage_" .. target:entindex() ), {
 		endTime = 0.25,
 		callback = function()
-			-- Adjust damage based on agility of caster
-			local agility = caster:GetAgility()
-			local percent = ability:GetLevelSpecialValueFor( "trueshot_ranged_damage", ability:GetLevel() - 1 )
-			local damage = math.floor( agility * percent / 100 )
-			
-			-- check if unit has attribute
-			if not target.TrueshotDamage then
-				target.TrueshotDamage = 0
-			end
-			
-			-- Check if unit doesn't have buff
-			if not target:HasModifier( "modifier_trueshot_effect_datadriven" ) then
-				damage = 0
-			end
-			
-			local damage_ref = damage
-			
-			-- If the stored value is different
-			if target.TrueshotDamage ~= damage then
-				-- modifier values
-				local bitTable = { 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 }
+			if target and IsValidEntity(target) then
+				-- Adjust damage based on agility of caster
+				local agility = caster:GetAgility()
+				local percent = ability:GetLevelSpecialValueFor( "trueshot_ranged_damage", ability:GetLevel() - 1 )
+				local damage = math.floor( agility * percent / 100 )
 				
-				-- Get the list of modifiers on the hero and loops through removing
-				local modCount = target:GetModifierCount()
-				for i = 0, modCount do
-					for u = 1, #bitTable do
-						local val = bitTable[u]
-						if target:GetModifierNameByIndex( i ) == prefix .. val then
-							target:RemoveModifierByName( prefix .. val )
+				-- check if unit has attribute
+				if not target.TrueshotDamage then
+					target.TrueshotDamage = 0
+				end
+				
+				-- Check if unit doesn't have buff
+				if not target:HasModifier( "modifier_trueshot_effect_datadriven" ) then
+					damage = 0
+				end
+				
+				local damage_ref = damage
+				
+				-- If the stored value is different
+				if target.TrueshotDamage ~= damage then
+					-- modifier values
+					local bitTable = { 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 }
+					
+					-- Get the list of modifiers on the hero and loops through removing
+					local modCount = target:GetModifierCount()
+					for i = 0, modCount do
+						for u = 1, #bitTable do
+							local val = bitTable[u]
+							if target:GetModifierNameByIndex( i ) == prefix .. val then
+								target:RemoveModifierByName( prefix .. val )
+							end
+						end
+					end
+					
+					-- Add modifiers
+					for p = 1, #bitTable do
+						local val = bitTable[p]
+						local count = math.floor( damage / val )
+						if count >= 1 then
+							ability:ApplyDataDrivenModifier( caster, target, prefix .. val, {} )
+							damage = damage - val
 						end
 					end
 				end
-				
-				-- Add modifiers
-				for p = 1, #bitTable do
-					local val = bitTable[p]
-					local count = math.floor( damage / val )
-					if count >= 1 then
-						ability:ApplyDataDrivenModifier( caster, target, prefix .. val, {} )
-						damage = damage - val
-					end
-				end
+				target.TrueshotDamage = damage_ref
+				return 0.25
+			else
+				return nil
 			end
-			target.TrueshotDamage = damage_ref
-			return 0.25
 		end
 	})
 end
--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_troll_warlord/whirling_axes_melee.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_troll_warlord/whirling_axes_melee.lua
@@ -20,3 +20,91 @@ function LevelUpAbility( event )
 		ability_handle:SetLevel(this_abilityLevel)
 	end
 end
+
+function WhirlingAxesMelee( keys )
+	local caster = keys.caster
+	local caster_location = caster:GetAbsOrigin()
+	local ability = keys.ability
+	local forward_vector = caster:GetForwardVector()
+	local front_position = caster_location + forward_vector * 1
+	local axe_projectile = keys.axe_projectile
+	local axe_modifier = keys.axe_modifier
+
+	local angle_east = QAngle(0,-90,0)
+	local angle_west = QAngle(0,90,0)
+
+	local position_east = RotatePosition(caster_location, angle_east, front_position) 
+	local position_west = RotatePosition(caster_location, angle_west, front_position)
+
+	-- East axe
+	if caster.whirling_axes_east and IsValidEntity(caster.whirling_axes_east) then
+		caster.whirling_axes_east:RemoveSelf()
+	end
+
+	-- Create the axe
+	caster.whirling_axes_east = CreateUnitByName("npc_dota_troll_warlord_axe", position_east, false, caster, caster, caster:GetTeam() )
+	ability:ApplyDataDrivenModifier(caster, caster.whirling_axes_east, axe_modifier, {})
+
+	-- Set the particle
+	local particle_east = ParticleManager:CreateParticle(axe_projectile, PATTACH_ABSORIGIN_FOLLOW, caster.whirling_axes_east)
+	ParticleManager:SetParticleControlEnt(particle_east, 1, caster.whirling_axes_east, PATTACH_POINT_FOLLOW, "attach_hitloc", caster.whirling_axes_east:GetAbsOrigin(), true)
+	ParticleManager:SetParticleControl(particle_east, 4, Vector(5,0,0))
+
+	caster.whirling_axes_east.particle = particle_east
+	caster.whirling_axes_east.axe_radius = 1
+	caster.whirling_axes_east.start_time = GameRules:GetGameTime()
+	caster.whirling_axes_east.side = 0
+
+	-- West axe
+	if caster.whirling_axes_west and IsValidEntity(caster.whirling_axes_west) then
+		caster.whirling_axes_west:RemoveSelf()
+	end
+
+	-- Create the axe
+	caster.whirling_axes_west = CreateUnitByName("npc_dota_troll_warlord_axe", position_west, false, caster, caster, caster:GetTeam() )
+	ability:ApplyDataDrivenModifier(caster, caster.whirling_axes_west, axe_modifier, {})
+	
+	-- Set the particle
+	local particle_west = ParticleManager:CreateParticle(axe_projectile, PATTACH_ABSORIGIN_FOLLOW, caster.whirling_axes_west)
+	ParticleManager:SetParticleControlEnt(particle_west, 1, caster.whirling_axes_west, PATTACH_POINT_FOLLOW, "attach_hitloc", caster.whirling_axes_west:GetAbsOrigin(), true)
+	ParticleManager:SetParticleControl(particle_west, 4, Vector(5,0,0))
+
+	caster.whirling_axes_west.particle = particle_west
+	caster.whirling_axes_west.axe_radius = 1
+	caster.whirling_axes_west.start_time = GameRules:GetGameTime()
+	caster.whirling_axes_west.side = 1
+end
+
+function WhirlingAxesMeleeThink( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local caster_location = caster:GetAbsOrigin()
+	local elapsed_time = GameRules:GetGameTime() - target.start_time
+
+
+	
+
+
+	--------------------------------------------------------------------------------
+	-- Update the radius
+	--
+	local currentRadius	= target.axe_radius
+	local deltaRadius = 7
+	currentRadius = currentRadius + deltaRadius
+	currentRadius = math.min( math.max( currentRadius, 1 ), 350 )
+
+	print(currentRadius)
+
+	target.axe_radius = currentRadius
+
+	local rotation_angle = elapsed_time * 360 -- need to use time for this
+	local relPos = Vector( 0, currentRadius, 0 )
+	relPos = RotatePosition( Vector(0,0,0), QAngle( 0, -rotation_angle, 0 ), relPos )
+
+	local absPos = GetGroundPosition( relPos + caster_location, target )
+	target:SetAbsOrigin( absPos )
+
+	if elapsed_time >= 3 then
+		target:RemoveSelf()
+	end
+end
