917d959da44dd7dd746fc52737cb5c2a9ad4a96d
Martin Noya <martinnoya@gmail.com>
2015-03-26 15:05:21 -0300

    Minor dagon particle adjustment
    Was doing 500 to 900 instead of 400 to 800

--- game/dota_addons/spelllibrary/scripts/vscripts/items/item_dagon.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/items/item_dagon.lua
@@ -9,7 +9,7 @@ function item_dagon_datadriven_on_spell_start(keys)
 	
 	local dagon_particle = ParticleManager:CreateParticle("particles/items_fx/dagon.vpcf",  PATTACH_ABSORIGIN_FOLLOW, keys.caster)
 	ParticleManager:SetParticleControlEnt(dagon_particle, 1, keys.target, PATTACH_POINT_FOLLOW, "attach_hitloc", keys.target:GetAbsOrigin(), false)
-	local particle_effect_intensity = 400 + (100 * dagon_level)  --Control Point 2 in Dagon's particle effect takes a number between 400 and 800, depending on its level.
+	local particle_effect_intensity = 300 + (100 * dagon_level)  --Control Point 2 in Dagon's particle effect takes a number between 400 and 800, depending on its level.
 	ParticleManager:SetParticleControl(dagon_particle, 2, Vector(particle_effect_intensity))
 	
 	keys.caster:EmitSound("DOTA_Item.Dagon.Activate")
