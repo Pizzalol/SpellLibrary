9af1016134c0b12e22a0696571e526da46a0d31b
Pizzalol <siprak214@hotmail.com>
2015-04-13 18:50:40 +0200

    Finished Cold Snap

---
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_invoker/cold_snap.lua
@@ -0,0 +1,58 @@
+
+function ColdSnapStart( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+	local quas_level = caster:FindAbilityByName("invoker_quas_datadriven"):GetLevel() - 1
+
+	local duration = ability:GetLevelSpecialValueFor("duration", quas_level) 
+	local freeze_duration = ability:GetLevelSpecialValueFor("freeze_duration", quas_level) 
+	local freeze_cooldown = ability:GetLevelSpecialValueFor("freeze_cooldown", quas_level) 
+	local freeze_damage = ability:GetLevelSpecialValueFor("freeze_damage", quas_level)
+	local cooldown_modifier = keys.cooldown_modifier
+	local stun_modifier = keys.stun_modifier
+	local cold_snap_modifier = keys.cold_snap_modifier
+
+	local damage_table = {}
+	damage_table.attacker = caster
+	damage_table.victim = target
+	damage_table.ability = ability
+	damage_table.damage_type = ability:GetAbilityDamageType() 
+	damage_table.damage = freeze_damage
+
+	ability:ApplyDataDrivenModifier(caster, target, cold_snap_modifier, {Duration = duration}) 
+	ability:ApplyDataDrivenModifier(caster, target, stun_modifier, {Duration = freeze_duration}) 
+	ability:ApplyDataDrivenModifier(caster, target, cooldown_modifier, {Duration = freeze_cooldown}) 
+
+	ApplyDamage(damage_table)
+end
+
+function ColdSnapDamage( keys )
+	local caster = keys.caster
+	local target = keys.unit
+	local ability = keys.ability
+	local quas_level = caster:FindAbilityByName("invoker_quas_datadriven"):GetLevel() - 1
+
+	local damage_taken = keys.DamageTaken
+	local freeze_duration = ability:GetLevelSpecialValueFor("freeze_duration", quas_level) 
+	local freeze_cooldown = ability:GetLevelSpecialValueFor("freeze_cooldown", quas_level) 
+	local freeze_damage = ability:GetLevelSpecialValueFor("freeze_damage", quas_level)
+	local damage_trigger = ability:GetLevelSpecialValueFor("damage_trigger", quas_level)
+	local cooldown_modifier = keys.cooldown_modifier
+	local stun_modifier = keys.stun_modifier
+
+	if damage_taken >= damage_trigger and not target:HasModifier(cooldown_modifier) then
+		local damage_table = {}
+		damage_table.attacker = caster
+		damage_table.victim = target
+		damage_table.ability = ability
+		damage_table.damage_type = ability:GetAbilityDamageType() 
+		damage_table.damage = freeze_damage
+
+		ability:ApplyDataDrivenModifier(caster, target, stun_modifier, {Duration = freeze_duration}) 
+		ability:ApplyDataDrivenModifier(caster, target, cooldown_modifier, {Duration = freeze_cooldown})
+
+		ApplyDamage(damage_table)
+	end
+end
