1388f33d654bb04714aec0956cc2d32f36e1f3c5
MNoya <martinnoya@gmail.com>
2015-04-20 14:20:47 -0300

    Finished Clinkz

--- game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_clinkz/death_pact.lua
+++ game/dota_addons/spelllibrary/scripts/vscripts/heroes/hero_clinkz/death_pact.lua
@@ -7,35 +7,40 @@ function DeathPact( event )
 	local caster = event.caster
 	local target = event.target
 	local ability = event.ability
+	local duration = ability:GetLevelSpecialValueFor( "duration" , ability:GetLevel() - 1 )
 
 	-- Health Gain
 	local health_gain_pct = ability:GetLevelSpecialValueFor( "health_gain_pct" , ability:GetLevel() - 1 ) * 0.01
 	local target_health = target:GetHealth()
-	local health_gain = target_health * health_gain_pct
+	local health_gain = math.floor(target_health * health_gain_pct)
+
+	local health_modifier = "modifier_death_pact_health"
+	ability:ApplyDataDrivenModifier(caster, caster, health_modifier, { duration = duration })
+	caster:SetModifierStackCount( health_modifier, ability, health_gain )
+	caster:Heal( health_gain, caster)
 
 	-- Damage Gain
 	local damage_gain_pct = ability:GetLevelSpecialValueFor( "damage_gain_pct" , ability:GetLevel() - 1 ) * 0.01
-	local damage_gain = target_health * health_gain_pct
+	local damage_gain = math.floor(target_health * damage_gain_pct)
 
-	-- TODO: Test if this sticks with item/level change or needs bitfield shit
-	ability.health_gain = health_gain
-	caster:SetMaxHealth(caster:GetMaxHealth() + health_gain)
-	caster:Heal( health_gain, caster)
-
-	ability.damage_gain = damage_gain
-	caster:SetBaseDamageMax(caster:GetBaseDamageMax() + damage_gain)
-	caster:SetBaseDamageMin(caster:GetBaseDamageMin() + damage_gain)
+	local damage_modifier = "modifier_death_pact_damage"
+	ability:ApplyDataDrivenModifier(caster, caster, damage_modifier, { duration = duration })
+	caster:SetModifierStackCount( damage_modifier, ability, damage_gain )
 
 	target:Kill(ability, caster)
+
+	print("Gained "..damage_gain.." damage and  "..health_gain.." health")
+	caster.death_pact_health = health_gain
 end
 
--- When the duration ends, max HP returns to normal, but current HP stays the same.
-function DeathPactEnd( event )
-	local health_gain = ability.health_gain
-	local damage_gain = ability.damage_gain
-	caster:SetMaxHealth(caster:GetMaxHealth() - health_gain)
-	caster:SetBaseDamageMax(caster:GetBaseDamageMax() - damage_gain)
-	caster:SetBaseDamageMin(caster:GetBaseDamageMin() - damage_gain)
+-- Keeps track of the casters health
+function DeathPactHealth( event )
+	local caster = event.caster
+	caster.OldHealth = caster:GetHealth()
 end
 
--- TODO: Check interaction with Refresher
+-- Sets the current health to the old health
+function SetCurrentHealth( event )
+	local caster = event.caster
+	caster:SetHealth(caster.OldHealth)
+end
