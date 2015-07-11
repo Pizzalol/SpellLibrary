e118a71fa741f971cb4ea076459687b09dc320e8
Pizzalol <siprak214@hotmail.com>
2015-07-11 18:50:14 +0200

    Update Necro Heartstopper aura to act like HP removal damage

--- game/scripts/vscripts/heroes/hero_necrolyte/heartstopper_aura.lua
+++ game/scripts/vscripts/heroes/hero_necrolyte/heartstopper_aura.lua
@@ -1,5 +1,5 @@
 --[[Author: Pizzalol
-	Date: 05.01.2015.
+	Date: 11.07.2015.
 	Deals damage based on the max HP of the target]]
 function HeartstopperAura( keys )
 	local caster = keys.caster
@@ -24,6 +24,7 @@ function HeartstopperAura( keys )
 	damage_table.damage_type = DAMAGE_TYPE_PURE
 	damage_table.ability = ability
 	damage_table.damage = target_max_hp * -aura_damage * aura_damage_interval
+	damage_table.damage_flags = DOTA_DAMAGE_FLAG_HPLOSS -- Doesnt trigger abilities and items that get disabled by damage
 
 	ApplyDamage(damage_table)
 end
