0877c7746c6598b02e5dccdb9cd2739a3ec4eb98
Pizzalol <siprak214@hotmail.com>
2015-07-12 18:14:03 +0200

    Removed extra projectile from Vengeful Wave of Terror

--- game/scripts/vscripts/heroes/hero_vengefulspirit/wave_of_terror.lua
+++ game/scripts/vscripts/heroes/hero_vengefulspirit/wave_of_terror.lua
@@ -1,5 +1,5 @@
 --[[Author: Pizzalol, kritth
-	Date: 31.01.2015.
+	Date: 12.07.2015.
 	Provides vision along the way of the projectile]]
 function WaveOfTerrorVision( keys )
 	local caster = keys.caster
@@ -13,6 +13,7 @@ function WaveOfTerrorVision( keys )
 	local wave_width = ability:GetLevelSpecialValueFor("wave_width", (ability:GetLevel() - 1))
 	local wave_range = ability:GetLevelSpecialValueFor("wave_range", (ability:GetLevel() - 1))
 	local wave_location = caster_location
+	local wave_particle = keys.wave_particle
 
 	-- Vision variables
 	local vision_aoe = ability:GetLevelSpecialValueFor("vision_aoe", (ability:GetLevel() - 1))
@@ -21,39 +22,38 @@ function WaveOfTerrorVision( keys )
 	-- Creating the projectile
 	local projectileTable =
 	{
-		EffectName = "",
-		Ability = nil,
+		EffectName = wave_particle,
+		Ability = ability,
 		vSpawnOrigin = caster_location,
 		vVelocity = Vector( forwardVec.x * wave_speed, forwardVec.y * wave_speed, 0 ),
-		fDistance = 99999,
+		fDistance = wave_range,
 		fStartRadius = wave_width,
 		fEndRadius = wave_width,
 		Source = caster,
 		bHasFrontalCone = false,
 		bReplaceExisting = false,
 		iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
-		iUnitTargetFlags = DOTA_UNIT_TARGET_NONE,
-		iUnitTargetType = DOTA_UNIT_TARGET_HERO
+		iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+		iUnitTargetType = DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO
 	}
 	-- Saving the projectile ID so that we can destroy it later
 	projectile_id = ProjectileManager:CreateLinearProjectile( projectileTable )
 
-	-- Timer to provide vision and to destroy the projectile
+	-- Timer to provide vision
 	Timers:CreateTimer( function()
 		-- Calculating the distance traveled
 		wave_location = wave_location + forwardVec * (wave_speed * 1/30)
 
-		ability:CreateVisibilityNode( wave_location, vision_aoe, vision_duration )
+		-- Reveal the area after the projectile passes through it
+		AddFOWViewer(caster:GetTeamNumber(), wave_location, vision_aoe, vision_duration, false)
 
 		local distance = (wave_location - caster_location):Length2D()
 
-		-- Checking if we traveled far enough, if yes then destroy the projectile and kill the timer
+		-- Checking if we traveled far enough, if yes then destroy the timer
 		if distance >= wave_range then
-			ProjectileManager:DestroyLinearProjectile(projectile_id)
 			return nil
 		else
 			return 1/30
 		end
 	end)
-
 end
