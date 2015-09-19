d6f3ac1ed2f4752bb6f5fcc371604e8919e3483d
Pizzalol <siprak214@hotmail.com>
2015-09-19 14:46:46 +0200

    Updated the Hide/Show wearables method

--- game/scripts/vscripts/heroes/hero_shadow_shaman/voodoo.lua
+++ game/scripts/vscripts/heroes/hero_shadow_shaman/voodoo.lua
@@ -30,55 +30,30 @@ function voodoo_end( keys )
 	end
 end
 
+
 --[[Author: Noya
-	Date: 10.01.2015.
+	Date: 09.08.2015.
 	Hides all dem hats
 ]]
 function HideWearables( event )
 	local hero = event.target
 	local ability = event.ability
-	local duration = ability:GetLevelSpecialValueFor( "duration", ability:GetLevel() - 1 )
-	print("Hiding Wearables")
-	--hero:AddNoDraw() -- Doesn't work on classname dota_item_wearable
 
-	hero.wearableNames = {} -- In here we'll store the wearable names to revert the change
-	hero.hiddenWearables = {} -- Keep every wearable handle in a table, as its way better to iterate than in the MovePeer system
+	hero.hiddenWearables = {} -- Keep every wearable handle in a table to show them later
     local model = hero:FirstMoveChild()
     while model ~= nil do
-        if model:GetClassname() ~= "" and model:GetClassname() == "dota_item_wearable" then
-            local modelName = model:GetModelName()
-            if string.find(modelName, "invisiblebox") == nil then
-            	-- Add the original model name to revert later
-            	table.insert(hero.wearableNames,modelName)
-            	print("Hidden "..modelName.."")
-
-            	-- Set model invisible
-            	model:SetModel("models/development/invisiblebox.vmdl")
-            	table.insert(hero.hiddenWearables,model)
-            end
+        if model:GetClassname() == "dota_item_wearable" then
+            model:AddEffects(EF_NODRAW) -- Set model hidden
+            table.insert(hero.hiddenWearables, model)
         end
         model = model:NextMovePeer()
-        if model ~= nil then
-        	print("Next Peer:" .. model:GetModelName())
-        end
     end
 end
 
---[[Author: Noya
-	Date: 10.01.2015.
-	Shows the hidden hero wearables
-]]
 function ShowWearables( event )
 	local hero = event.target
-	print("Showing Wearables on ".. hero:GetModelName())
 
-	-- Iterate on both tables to set each item back to their original modelName
-	for i,v in ipairs(hero.hiddenWearables) do
-		for index,modelName in ipairs(hero.wearableNames) do
-			if i==index then
-				print("Changed "..v:GetModelName().. " back to "..modelName)
-				v:SetModel(modelName)
-			end
-		end
+	for i,v in pairs(hero.hiddenWearables) do
+		v:RemoveEffects(EF_NODRAW)
 	end
 end
