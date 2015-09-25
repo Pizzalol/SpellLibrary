4ccb0e7c425f0f2e6d76f1bdb7ecd7aecea613ab
Pizzalol <siprak214@hotmail.com>
2015-09-25 23:35:57 +0200

    Pudge lua abilities
    Still need to comment the code

---
+++ game/scripts/vscripts/heroes/hero_pudge/Modifiers/modifier_meat_hook_followthrough_lua.lua
@@ -0,0 +1,20 @@
+modifier_meat_hook_followthrough_lua = class({})
+
+--------------------------------------------------------------------------------
+
+function modifier_meat_hook_followthrough_lua:IsHidden()
+	return true
+end
+
+
+--------------------------------------------------------------------------------
+
+function modifier_meat_hook_followthrough_lua:CheckState()
+	local state = {
+	[MODIFIER_STATE_STUNNED] = true,
+	}
+
+	return state
+end
+
+--------------------------------------------------------------------------------
--- game/scripts/vscripts/heroes/hero_pudge/rot.lua
+++
@@ -1,9 +0,0 @@
---[[Author: Pizzalol
-	Date: 1.1.2015.
-	Stops the rot sound]]
-function RotStopSound( keys )
-	local caster = keys.caster
-	local sound = "Hero_Pudge.Rot"
-
-	StopSoundEvent(sound, caster)
-end
