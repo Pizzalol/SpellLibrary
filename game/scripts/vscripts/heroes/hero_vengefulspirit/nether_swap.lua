80ce43294b1006774fa48590d4efb27bf4c80ed8
Pizzalol <siprak214@hotmail.com>
2015-09-29 09:27:37 +0200

    Fixed abilities with self targeting method
    Abaddon, Wisp, Phantom Assassin and Vengeful Spirit now use the "CastFilterRejectCaster" key to exclude self from being a valid target

--- game/scripts/vscripts/heroes/hero_vengefulspirit/nether_swap.lua
+++ game/scripts/vscripts/heroes/hero_vengefulspirit/nether_swap.lua
@@ -25,27 +25,3 @@ function NetherSwap( keys )
 	-- Stops the current action of the target
 	target:Interrupt()
 end
-
---[[
-	Author: igo95862, Noya
-	Used by: Pizzalol
-	Date: 26.01.2015.
-	Disallows self targeting by checking if the target is not the caster when the ability starts
-]]
-function NetherSwapPreCast( event )
-	local caster = event.caster
-	local target = event.target
-	local player = caster:GetPlayerOwner()
-	local pID = caster:GetPlayerOwnerID()
-
-	-- This prevents the spell from going off
-	if target == caster then
-		caster:Stop()
-
-		-- Play Error Sound
-		EmitSoundOnClient("General.CastFail_InvalidTarget_Hero", player)
-
-		-- This makes use of the Custom Error Flash module by zedor. https://github.com/zedor/CustomError
-		FireGameEvent( 'custom_error_show', { player_ID = pID, _error = "Ability can't target Self" } )
-	end
-end
--- game/scripts/vscripts/heroes/hero_wisp/tether.lua
+++ game/scripts/vscripts/heroes/hero_wisp/tether.lua
@@ -1,27 +1,4 @@
 --[[
-	Author: igo95862, Noya
-	Date: 14.1.2015.
-	Disallows self targeting by checking if the target is not the caster when the ability starts
-]]
-function TetherPreCast( event )
-	local caster = event.caster
-	local target = event.target
-	local player = caster:GetPlayerOwner()
-	local pID = caster:GetPlayerOwnerID()
-
-	-- This prevents the spell from going off
-	if target == caster then
-		caster:Stop()
-
-		-- Play Error Sound
-		EmitSoundOnClient("General.CastFail_InvalidTarget_Hero", player)
-
-		-- This makes use of the Custom Error Flash module by zedor. https://github.com/zedor/CustomError
-		FireGameEvent( 'custom_error_show', { player_ID = pID, _error = "Ability Can't Target Self" } )
-	end
-end
-
---[[
 	Author: Ractidous, with help from Noya
 	Date: 03.02.2015.
 	Initialize the slowed units list, and let the caster latch.
