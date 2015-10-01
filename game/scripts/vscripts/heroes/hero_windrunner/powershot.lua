12e0cb2231d58c3d1da7eae1f8ecd5294f30b28f
Pizzalol <siprak214@hotmail.com>
2015-10-01 23:57:02 +0200

    Windrunner fixes
    Fixed Powershot to work again
    Windrun has a sound with reborn
    Updated missing credits

--- game/scripts/vscripts/heroes/hero_windrunner/powershot.lua
+++ game/scripts/vscripts/heroes/hero_windrunner/powershot.lua
@@ -1,32 +1,49 @@
 --[[
-	CHANGELIST:
-	09.01.2015 - Standized variables
-]]
-
---[[
-	Author: kritth
-	Date: 5.1.2015.
-	Init: Initialize the damaage
+	Author: kritth, Pizzalol
+	Date: 01.10.2015.
+	Initialize the data we require for the ability
 ]]
 function powershot_initialize( keys )
-	keys.caster.powershot_damage_percent = 0.0
+	local caster = keys.caster
+	local caster_location = caster:GetAbsOrigin()
+	local ability = keys.ability	
+	local ability_level = ability:GetLevel() - 1
+	local point = keys.target_points[1]
+
+	-- Ability variables
+	ability.powershot_damage_percent = 0.0
+	ability.powershot_traveled = 0
+	ability.powershot_direction = (point - caster_location):Normalized()
+	ability.powershot_source = caster_location
+	ability.powershot_currentPos = caster_location
+	ability.powershot_percent_movespeed = 100
+	ability.powershot_units_array = {}
+	ability.powershot_units_hit = {}
+
+	ability.powershot_interval_damage =  ability:GetLevelSpecialValueFor("damage_per_interval", ability_level)
+	ability.powershot_max_range = ability:GetLevelSpecialValueFor( "arrow_range", ability_level )
+	ability.powershot_max_movespeed = ability:GetLevelSpecialValueFor( "arrow_speed", ability_level )
+	ability.powershot_radius = ability:GetLevelSpecialValueFor( "arrow_width", ability_level )
+	ability.powershot_vision_radius = ability:GetLevelSpecialValueFor( "vision_radius", ability_level )	
+	ability.powershot_vision_duration = ability:GetLevelSpecialValueFor( "vision_duration", ability_level )
+	ability.powershot_damage_reduction = ability:GetLevelSpecialValueFor( "damage_reduction", ability_level )
+	ability.powershot_speed_reduction = ability:GetLevelSpecialValueFor( "speed_reduction", ability_level )
+	ability.powershot_tree_width = ability:GetLevelSpecialValueFor("tree_width", ability_level) * 2 -- Double the radius because the original feels too small
 end
 
 --[[
 	Author: kritth
-	Date: 5.1.2015.
+	Date: 01.10.2015.
 	Init: Charge the damage per duration
 ]]
 function powershot_charge( keys )
-	-- Variables 
-	local caster = keys.caster
-	local specialValueName =  "damage_per_interval"
+	local ability = keys.ability
 	
 	-- Fail check
-	if not caster.powershot_damage_percent then
-		caster.powershot_damage_percent = 0.0
+	if not ability.powershot_damage_percent then
+		ability.powershot_damage_percent = 0.0
 	end
-	caster.powershot_damage_percent = caster.powershot_damage_percent + keys.ability:GetLevelSpecialValueFor( specialValueName, ( keys.ability:GetLevel() - 1 ) )
+	ability.powershot_damage_percent = ability.powershot_damage_percent + ability.powershot_interval_damage
 end
 
 --[[
@@ -37,53 +54,27 @@ end
 function powershot_register_unit( keys )
 	-- Variables
 	local caster = keys.caster
+	local ability = keys.ability
 	local target = keys.target
 	local index = keys.target:entindex()
 	
 	-- Register
-	caster.powershot_units_array[ index ] = keys.target
-	caster.powershot_units_hit[ index ] = false
+	ability.powershot_units_array[ index ] = target
+	ability.powershot_units_hit[ index ] = false
 end
 
 --[[
-	Author: kritth
-	Date: 5.1.2015.
+	Author: kritth, Pizzalol
+	Date: 01.10.2015.
 	Main: Start traversing upon timer while providing vision, reducing damage and speed per units hit, and also destroy trees
-	Changes:
-	09.01.2015 - Minor cleanup
-	08.03.2015 - Minor fix for extremely fast speed
 ]]
 function powershot_start_traverse( keys )
 	-- Variables
 	local caster = keys.caster
 	local ability = keys.ability
-	local point = keys.target_points[ 1 ]
-	local forwardVec = point - caster:GetAbsOrigin()
-	local dummyModifierName = "modifier_powershot_dummy_datadriven"
 	local startAttackSound = "Ability.PowershotPull"
 	local startTraverseSound = "Ability.Powershot"
 	local projectileName = "particles/units/heroes/hero_windrunner/windrunner_spell_powershot.vpcf"
-	
-	-- Necessary variables from KV
-	local max_range = ability:GetLevelSpecialValueFor( "arrow_range", ( ability:GetLevel() - 1 ) )
-	local max_movespeed = ability:GetLevelSpecialValueFor( "arrow_speed", ( ability:GetLevel() - 1 ) )
-	local radius = ability:GetLevelSpecialValueFor( "arrow_width", ( ability:GetLevel() - 1 ) )
-	local vision_radius = ability:GetLevelSpecialValueFor( "vision_radius", ( ability:GetLevel() - 1 ) )
-	
-	-- Variables to use in timer
-	caster.powershot_movespeed = max_movespeed							-- Store the maximum movespeed
-	caster.powershot_percent_movespeed = 100							-- Store current movement speed
-	caster.powershot_source = caster:GetAbsOrigin()						-- Store the source position of the damage
-	caster.powershot_currentPos = caster:GetAbsOrigin()					-- Store current position the damage is at
-	caster.powershot_max_range = max_range								-- Maximum range it can hit
-	caster.powershot_radius = radius
-	caster.powershot_forwardVec = forwardVec:Normalized()				-- Forward vector
-	caster.powershot_units_array = {}									-- Table to store units, key is entindex, value is entity
-	caster.powershot_units_hit = {}										-- Flag to check if unit is hit, key is entindex, value is true or false
-	caster.powershot_vision_radius = vision_radius
-	caster.powershot_vision_duration = ability:GetLevelSpecialValueFor( "vision_duration", ( ability:GetLevel() - 1 ) )
-	caster.powershot_damage_reduction = ability:GetLevelSpecialValueFor( "damage_reduction", ( ability:GetLevel() - 1 ) )
-	caster.powershot_speed_reduction = ability:GetLevelSpecialValueFor( "speed_reduction", ( ability:GetLevel() - 1 ) )
 	
 	-- Stop sound event and fire new one, can do this in datadriven but for continuous purpose, let's put it here
 	StopSoundEvent( startAttackSound, caster )
@@ -94,25 +85,24 @@ function powershot_start_traverse( keys )
 	{
 		EffectName = projectileName,
 		Ability = ability,
-		vSpawnOrigin = caster:GetAbsOrigin(),
-		vVelocity = Vector( caster.powershot_forwardVec.x * max_movespeed,
-			caster.powershot_forwardVec.y * max_movespeed, 0 ),
-		fDistance = 99999,
-		fStartRadius = radius,
-		fEndRadius = radius,
+		vSpawnOrigin = ability.powershot_source,
+		vVelocity = Vector(ability.powershot_direction.x * ability.powershot_max_movespeed, ability.powershot_direction.y * ability.powershot_max_movespeed, 0),
+		fDistance = ability.powershot_max_range,
+		fStartRadius = ability.powershot_radius,
+		fEndRadius = ability.powershot_radius,
 		Source = caster,
 		bHasFrontalCone = false,
 		bReplaceExisting = true,
 		iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
 		iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
 		iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
-		iVisionRadius = vision_radius,
+		iVisionRadius = ability.powershot_vision_radius,
 		iVisionTeamNumber = caster:GetTeamNumber()
 	}
 	caster.powershot_projectileID = ProjectileManager:CreateLinearProjectile( projectileTable )
 	
 	-- Register units around caster
-	local units = FindUnitsInRadius( caster:GetTeamNumber(), caster:GetAbsOrigin(), caster, radius,
+	local units = FindUnitsInRadius( caster:GetTeamNumber(), caster:GetAbsOrigin(), caster, ability.powershot_radius,
 			DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, 0, false )
 	for k, v in pairs( units ) do
 		local index = v:entindex()
@@ -123,54 +113,46 @@ function powershot_start_traverse( keys )
 	-- Traverse
 	Timers:CreateTimer( function()
 			-- Traverse the point
-			caster.powershot_currentPos = caster.powershot_currentPos + ( caster.powershot_forwardVec * caster.powershot_percent_movespeed * caster.powershot_movespeed * 0.03 )
+			ability.powershot_currentPos = ability.powershot_currentPos + ( ability.powershot_direction * ability.powershot_percent_movespeed/100 * ability.powershot_max_movespeed * 1/30 )
+			ability.powershot_traveled = ability.powershot_traveled + ability.powershot_max_movespeed * 1/30
 			
 			-- Loop through the units array
-			for k, v in pairs( caster.powershot_units_array ) do
+			for k, v in pairs( ability.powershot_units_array ) do
 				-- Check if it never got hit and is in radius
-				if caster.powershot_units_hit[ k ] == false
-						and powershot_distance( v:GetAbsOrigin(), caster.powershot_currentPos ) <= caster.powershot_radius then
+				if ability.powershot_units_hit[ k ] == false and powershot_distance( v:GetAbsOrigin(), ability.powershot_currentPos ) <= ability.powershot_radius then
 					-- Deal damage
 					local damageTable =
 					{
 						victim = v,
 						attacker = caster,
-						damage = ability:GetAbilityDamage() * caster.powershot_damage_percent,
+						damage = ability:GetAbilityDamage() * ability.powershot_damage_percent,
 						damage_type = ability:GetAbilityDamageType()
 					}
 					ApplyDamage( damageTable )
 					-- Reduction
-					caster.powershot_damage_percent = caster.powershot_damage_percent * ( 1.0 - caster.powershot_damage_reduction )
-					caster.powershot_percent_movespeed = caster.powershot_percent_movespeed * ( 1.0 - caster.powershot_speed_reduction )
+					ability.powershot_damage_percent = ability.powershot_damage_percent * ( 1.0 - ability.powershot_damage_reduction )
+					ability.powershot_percent_movespeed = ability.powershot_percent_movespeed * ( 1.0 - ability.powershot_speed_reduction )
 					-- Change flag
-					caster.powershot_units_hit[ k ] = true
+					ability.powershot_units_hit[ k ] = true
 					-- Fire sound
 					StartSoundEvent( "Hero_Windrunner.PowershotDamage", v )
 				end
 			end
 			
-			-- Check if nearby tree, then create dummy to destroy them
-			if GridNav:IsNearbyTree( caster.powershot_currentPos, caster.powershot_radius, true ) then
-				local dummy = CreateUnitByName( "npc_dummy_blank", caster.powershot_currentPos, false, caster, caster, caster:GetTeamNumber() )
-				ability:ApplyDataDrivenModifier( caster, dummy, dummyModifierName, {} )
-				Timers:CreateTimer( 0.1, function()
-					dummy:ForceKill( true )
-					return nil
-				end )
+			-- Check for nearby trees, destroy them if they exist
+			if GridNav:IsNearbyTree( ability.powershot_currentPos, ability.powershot_radius, true ) then
+				GridNav:DestroyTreesAroundPoint(ability.powershot_currentPos, ability.powershot_tree_width, false)
 			end
 			
 			-- Create visibility node
-			ability:CreateVisibilityNode( caster.powershot_currentPos, caster.powershot_vision_radius, caster.powershot_vision_duration )
+			AddFOWViewer(caster:GetTeamNumber(), ability.powershot_currentPos, ability.powershot_vision_radius, ability.powershot_vision_duration, false)
 			
-			-- Check if damage point reach the maximum range, if so, delete the projectile and the timer
-			local dx = caster.powershot_source.x - caster.powershot_currentPos.x
-			local dy = caster.powershot_source.y - caster.powershot_currentPos.y
-			if math.sqrt( dx * dx + dy * dy ) > caster.powershot_max_range then
-				ProjectileManager:DestroyLinearProjectile( caster.powershot_projectileID )
-				return nil
+			-- Check if damage point reach the maximum range, if so, delete the timer
+			if ability.powershot_traveled < ability.powershot_max_range then
+				return 1/30
 			else
-				return 0.03
-			end	
+				return nil
+			end
 		end
 	)
 end
