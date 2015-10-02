80edc821527fd5f129e7def90d51b5d8dd0ff48a
Pizzalol <siprak214@hotmail.com>
2015-10-02 12:00:36 +0200

    Windrunner Shackleshot complete
    Shackleshot latches to trees now
    Shackleshot latching particle works now

--- game/scripts/vscripts/heroes/hero_windrunner/shackleshot.lua
+++ game/scripts/vscripts/heroes/hero_windrunner/shackleshot.lua
@@ -1,23 +1,21 @@
 --[[
-    author: jacklarnes
+    author: jacklarnes, Pizzalol
     email: christucket@gmail.com
     reddit: /u/jacklarnes
-]]
+    Date: 02.10.2015.
 
---[[
-    projectile latch particle doesn't work correctly
+    Finds valid units and trees to latch to if there are any then applies the particle effect and stun duration
 ]]
 function shackleshot_hit( keys )
     local caster = keys.caster
     local target = keys.target
     local ability = keys.ability
-
-    local debug = false
+    local ability_level = ability:GetLevel() - 1
 
     -- Ability variables
-    local stun_duration_long = ability:GetLevelSpecialValueFor("stun_duration", ability:GetLevel() - 1)
-    local stun_duration_fail = ability:GetLevelSpecialValueFor("fail_stun_duration", ability:GetLevel() - 1)
-    local shackle_distance = ability:GetLevelSpecialValueFor("shackle_distance", ability:GetLevel() - 1)
+    local stun_duration_long = ability:GetLevelSpecialValueFor("stun_duration", ability_level)
+    local stun_duration_fail = ability:GetLevelSpecialValueFor("fail_stun_duration", ability_level)
+    local shackle_distance = ability:GetLevelSpecialValueFor("shackle_distance", ability_level)
 
     -- Calculate the valid positions
     local caster_location = caster:GetAbsOrigin()
@@ -29,8 +27,8 @@ function shackleshot_hit( keys )
     local cone_left = RotatePosition(Vector(0,0,0), QAngle(0, -13, 0), shackle_vector_range)
     local cone_right = RotatePosition(Vector(0,0,0), QAngle(0, 13, 0), shackle_vector_range)
 
-
-    units = FindUnitsInRadius(caster:GetTeamNumber(),
+    -- Find valid units
+    local units = FindUnitsInRadius(caster:GetTeamNumber(),
                             target:GetAbsOrigin(),
                             nil,
                             shackle_distance,
@@ -40,50 +38,66 @@ function shackleshot_hit( keys )
                             FIND_ANY_ORDER,
                             false)
 
-    if debug then 
-        DebugDrawClear()
-        DebugDrawLine(Vector(0,0,0), cone_left, 0, 0, 255, true, 120)
-        DebugDrawLine(Vector(0,0,0), cone_right, 255, 0, 0, true, 120)
-    end
-
-    -- Choose a valid target is possible
+    -- Choose a valid target if possible
     local latch_target = nil
     for k,unit in pairs(units) do
         local unit_vector = unit:GetAbsOrigin() - target_point
 
         if (not vector_is_clockwise(cone_left, unit_vector)) and vector_is_clockwise(cone_right, unit_vector) then
-            if debug then DebugDrawCircle(unit_vector, Vector(0, 255, 0), 128, 5, true, 120) end
 
             if latch_target == nil then
                 latch_target = unit
+                break
             end
-
-            if not debug then break end
-        else
-            if debug then DebugDrawCircle(unit_vector, Vector(255, 255, 0), 128, 5, true, 120) end
         end
     end
 
-    -- Fire the sound and particle
-    -- Apply the stun modifier
-    -- NOTE: particle doesn't work currently
+    -- If a valid target has been found then create the particle, apply the stun to both targets and fire the sound effect
     if latch_target ~= nil then
         latch = ParticleManager:CreateParticle("particles/units/heroes/hero_windrunner/windrunner_shackleshot_pair.vpcf", PATTACH_CUSTOMORIGIN, caster)
-        --ParticleManager:SetParticleControl(latch, 0, target:GetAbsOrigin())
-        --ParticleManager:SetParticleControl(latch, 1, latch_target:GetAbsOrigin())
 
         ParticleManager:SetParticleControlEnt(latch, 0, target, PATTACH_POINT, "attach_hitloc", target:GetAbsOrigin(), true)
         ParticleManager:SetParticleControlEnt(latch, 1, latch_target, PATTACH_POINT, "attach_hitloc", latch_target:GetAbsOrigin(), true)
-        print("latch!")
+        ParticleManager:SetParticleControl(latch, 2, Vector(stun_duration_long,0,0))
 
         ability:ApplyDataDrivenModifier(caster, target, "modifier_shackle_stun_datadriven", {Duration = stun_duration_long})
         ability:ApplyDataDrivenModifier(caster, latch_target, "modifier_shackle_stun_datadriven", {Duration = stun_duration_long})
 
         EmitSoundOn("Hero_Windrunner.ShackleshotBind", target)
     else
-        ability:ApplyDataDrivenModifier(caster, target, "modifier_shackle_stun_datadriven", {Duration = stun_duration_fail})
+        -- Otherwise repeat the same steps and try to find a valid tree for latching
+        local latch_tree = nil
+        local trees = GridNav:GetAllTreesAroundPoint(target:GetAbsOrigin(), shackle_distance, false)
 
-        EmitSoundOn("Hero_Windrunner.ShackleshotStun", target)
+        for _,tree in pairs(trees) do
+            local tree_vector = tree:GetAbsOrigin() - target_point
+
+            if (not vector_is_clockwise(cone_left, tree_vector)) and vector_is_clockwise(cone_right, tree_vector) then
+
+                if latch_tree == nil then
+                    latch_tree = tree
+                    break
+                end
+            end
+        end
+
+        -- If a valid tree has been found then do the particle, stun and sound functions
+        if latch_tree then
+            latch = ParticleManager:CreateParticle("particles/units/heroes/hero_windrunner/windrunner_shackleshot_pair.vpcf", PATTACH_CUSTOMORIGIN, caster)
+
+            ParticleManager:SetParticleControlEnt(latch, 0, target, PATTACH_POINT, "attach_hitloc", target:GetAbsOrigin(), true)
+            ParticleManager:SetParticleControl(latch, 1, latch_tree:GetAbsOrigin() + Vector(0,0,128))
+            ParticleManager:SetParticleControl(latch, 2, Vector(stun_duration_long,0,0))
+
+            ability:ApplyDataDrivenModifier(caster, target, "modifier_shackle_stun_datadriven", {Duration = stun_duration_long})
+
+            EmitSoundOn("Hero_Windrunner.ShackleshotBind", target)
+        else
+            -- If no valid tree nor unit was found for latching then apply the short stun duration and play the failure sound
+            ability:ApplyDataDrivenModifier(caster, target, "modifier_shackle_stun_datadriven", {Duration = stun_duration_fail})
+
+            EmitSoundOn("Hero_Windrunner.ShackleshotStun", target)
+        end
     end
 end
 
