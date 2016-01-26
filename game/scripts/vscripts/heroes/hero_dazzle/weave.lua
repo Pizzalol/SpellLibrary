5d0cfe73df31dd61c6040199db4587a2717349e3
Pizzalol <siprak214@hotmail.com>
2016-01-26 17:59:20 +0100

    Dazzle conversion complete
    - Updated abilities to 6.86
    - Improved Weave code

--- game/scripts/vscripts/heroes/hero_dazzle/weave.lua
+++ game/scripts/vscripts/heroes/hero_dazzle/weave.lua
@@ -1,7 +1,8 @@
 --[[Author: Pizzalol
-	Date: 02.02.2015.
+	Date: 26.01.2016.
 	Creates vision over the area]]
 function WeaveVision( keys )
+	local caster = keys.caster
 	local target_point = keys.target_points[1]
 	local ability = keys.ability
 	local ability_level = ability:GetLevel() - 1
@@ -9,53 +10,17 @@ function WeaveVision( keys )
 	local vision_radius = ability:GetLevelSpecialValueFor("vision", ability_level)
 	local vision_duration = ability:GetLevelSpecialValueFor("vision_duration", ability_level)
 
-	ability:CreateVisibilityNode(target_point, vision_radius, vision_duration)
+	AddFOWViewer(caster:GetTeamNumber(),target_point,vision_radius,vision_duration,true)
 end
 
 --[[Author: Pizzalol
-	Date: 02.02.2015.
-	Removes the applied positive armor buffs from the target]]
-function WeaveRemovePositive( keys )
-	local caster = keys.caster
+	Date: 26.01.2016.
+	Removes the targeted modifier]]
+function WeaveRemoveModifier( keys )
 	local target = keys.target
-	local ability = keys.ability
-	local ability_level = ability:GetLevel() - 1
 	local modifier = keys.modifier
 
-	-- Modifier variables
-	local duration = ability:GetLevelSpecialValueFor("duration", ability_level)
-	local tick_interval = ability:GetLevelSpecialValueFor("tick_interval", ability_level)
-
-	-- Calculating how many modifiers we have to remove
-	local modifiers_to_remove = duration / tick_interval
-
-	-- Removing them
-	for i = 1, modifiers_to_remove do
-		target:RemoveModifierByNameAndCaster(modifier, caster)
-	end
-end
-
---[[Author: Pizzalol
-	Date: 02.02.2015.
-	Removes the applied negative armor buffs from the target]]
-function WeaveRemoveNegative( keys )
-	local caster = keys.caster
-	local target = keys.target
-	local ability = keys.ability
-	local ability_level = ability:GetLevel() - 1
-	local modifier = keys.modifier
-
-	-- Modifier variables
-	local duration = ability:GetLevelSpecialValueFor("duration", ability_level)
-	local tick_interval = ability:GetLevelSpecialValueFor("tick_interval", ability_level)
-
-	-- Calculating how many modifiers we have to remove
-	local modifiers_to_remove = duration / tick_interval
-
-	-- Removing them
-	for i = 1, modifiers_to_remove do
-		target:RemoveModifierByNameAndCaster(modifier, caster)
-	end
+	target:RemoveModifierByName(modifier)
 end
 
 --[[
@@ -85,17 +50,6 @@ function WeavePositiveParticle( event )
 		ParticleManager:SetParticleControl(target.WeavePositiveParticle, 1, target:GetAbsOrigin())
 
 		ParticleManager:SetParticleControlEnt(target.WeavePositiveParticle, 1, target, PATTACH_OVERHEAD_FOLLOW, "attach_overhead", target:GetAbsOrigin(), true)
-	end
-end
-
--- Destroys the particle when the modifier is destroyed, only when the target doesnt have the modifier
-function EndWeavePositiveParticle( event )
-	local target = event.target
-	local particleName = event.particle_name
-	local modifier = event.modifier
-
-	if not target:HasModifier(modifier) then
-		ParticleManager:DestroyParticle(target.WeavePositiveParticle,false)
 	end
 end
 
@@ -129,13 +83,36 @@ function WeaveNegativeParticle( event )
 	end
 end
 
--- Destroys the particle when the modifier is destroyed, only when the target doesnt have the modifier
-function EndWeaveNegativeParticle( event )
+--[[Author: Pizzalol
+	Date: 26.01.2016.
+	Destroys the particle when the modifier is destroyed, only when the target doesnt have the modifier]]
+function EndWeaveParticle( event )
 	local target = event.target
-	local particleName = event.particle_name
 	local modifier = event.modifier
+	local particle_type = event.particle_type -- 0 negative and 1 positive
 
 	if not target:HasModifier(modifier) then
-		ParticleManager:DestroyParticle(target.WeaveNegativeParticle,false)
+		if particle_type == 0 then
+			ParticleManager:DestroyParticle(target.WeaveNegativeParticle,false)
+		else
+			ParticleManager:DestroyParticle(target.WeavePositiveParticle,false)
+		end
+	end
+end
+
+--[[Author: Pizzalol
+	Date: 26.01.2016.
+	Increment the stack count]]
+function WeaveIncrement( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local modifier = keys.modifier
+
+	local current_stack = target:GetModifierStackCount(modifier,caster)
+
+	if current_stack < 1 then
+		target:SetModifierStackCount(modifier,caster,1)
+	else
+		target:SetModifierStackCount(modifier,caster,current_stack + 1)
 	end
 end
