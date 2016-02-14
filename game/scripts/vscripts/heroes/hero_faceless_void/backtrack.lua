6ebd9f59fe147b6edad11c00552d903b0eab5b62
Pizzalol <siprak214@hotmail.com>
2016-02-14 17:15:45 +0100

    Faceless Void conversion complete
    - Changed how Backtrack tracks health

--- game/scripts/vscripts/heroes/hero_faceless_void/backtrack.lua
+++ game/scripts/vscripts/heroes/hero_faceless_void/backtrack.lua
@@ -1,28 +1,23 @@
-
-healthTable = healthTable or {}
-
 --[[Author: Pizzalol
-	Date: 06.01.2015.
+	Date: 14.02.2016.
 	Keeps track of the casters health]]
 function BacktrackHealth( keys )
 	local caster = keys.caster
+	local ability = keys.ability
 
-	healthTable[caster] = healthTable[caster] or {}
+	ability.caster_hp_old = ability.caster_hp_old or caster:GetMaxHealth()
+	ability.caster_hp = ability.caster_hp or caster:GetMaxHealth()
 
-	if healthTable[caster].old == nil or healthTable[caster].new == nil then
-		healthTable[caster].old = caster:GetMaxHealth()
-		healthTable[caster].new = caster:GetMaxHealth()
-	end
-
-	healthTable[caster].old = healthTable[caster].new
-	healthTable[caster].new = caster:GetHealth()
+	ability.caster_hp_old = ability.caster_hp
+	ability.caster_hp = caster:GetHealth()
 end
 
 --[[Author: Pizzalol
-	Date: 06.01.2015.
+	Date: 14.02.2016.
 	Negates incoming damage]]
 function BacktrackHeal( keys )
 	local caster = keys.caster
+	local ability = keys.ability
 
-	caster:SetHealth(healthTable[caster].old)
+	caster:SetHealth(ability.caster_hp_old)
 end
