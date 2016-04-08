503d94a86b0b0891652a7881e5a2d9da8403c7ab
Kyler Conena <kylerownsx62@aim.com>
2016-04-08 00:49:29 -0400

    Made all projectiles fire at the same time
    Fixed the way projectiles travel after hitting the primary target, and made them nearly instant (not sure if damage should be instant or what the speed of the projectile should be)

--- game/scripts/vscripts/heroes/hero_templar_assassin/psi_blades.lua
+++ game/scripts/vscripts/heroes/hero_templar_assassin/psi_blades.lua
@@ -6,44 +6,34 @@ function CheckAngles(keys)
 	local target = keys.target
 	local ability = keys.ability
 	
-	-- Ensures this is the first attack (directly from the caster)
-	if ability.attacker_angle == nil then
-		-- Notes the origin of the first target to be the center of the findunits radius
-		ability.first_target_origin = target:GetAbsOrigin()
-		-- Notes the damage the first target takes to apply to the other targets
-		ability.first_target_damage = keys.damage
+	-- Notes the origin of the first target to be the center of the findunits radius
+	local first_target_origin = target:GetAbsOrigin()
+	-- Notes the damage the first target takes to apply to the other targets
+	ability.first_target_damage = keys.damage
 		
-		-- Gets the caster's origin difference from the target
-		local caster_origin_difference = caster:GetAbsOrigin() - ability.first_target_origin 
+	-- Gets the caster's origin difference from the target
+	local caster_origin_difference = caster:GetAbsOrigin() - first_target_origin 
 
-		-- Get the radian of the origin difference between the attacker and TA. We use this to figure out at what angle the victim is at relative to the TA.
-		local caster_origin_difference_radian = math.atan2(caster_origin_difference.y, caster_origin_difference.x)
+	-- Get the radian of the origin difference between the attacker and TA. We use this to figure out at what angle the victim is at relative to the TA.
+	local caster_origin_difference_radian = math.atan2(caster_origin_difference.y, caster_origin_difference.x)
 	
-		-- Convert the radian to degrees.
-		caster_origin_difference_radian = caster_origin_difference_radian * 180
-		ability.attacker_angle = caster_origin_difference_radian / math.pi
-		-- Turns negative angles into positive ones and make the math simpler.
-		ability.attacker_angle = ability.attacker_angle + 180.0
-	else
-		-- Applies the damage to the attack target
-		ApplyDamage({victim = target, attacker = caster, damage = ability.first_target_damage, damage_type = ability:GetAbilityDamageType()})
-	end
+	-- Convert the radian to degrees.
+	caster_origin_difference_radian = caster_origin_difference_radian * 180
+	local attacker_angle = caster_origin_difference_radian / math.pi
+	-- Turns negative angles into positive ones and make the math simpler.
+	attacker_angle = attacker_angle + 180.0
 	
 	local radius = ability:GetLevelSpecialValueFor("attack_spill_range", ability:GetLevel() - 1)
 	local attack_spill_width = ability:GetLevelSpecialValueFor("attack_spill_width", ability:GetLevel() - 1)/2
 	
 	-- Units in radius
-	local units = FindUnitsInRadius(caster:GetTeamNumber(), ability.first_target_origin, nil, radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), 0, 0, false)
+	local units = FindUnitsInRadius(caster:GetTeamNumber(), first_target_origin, nil, radius, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), 0, 0, false)
 	
-	local new_target
-	local closest = radius
 	-- Calculates the position of each found unit in relation to the last target
 	for i,unit in ipairs(units) do
 		if unit ~= target then
 		
 			local target_origin_difference = target:GetAbsOrigin() - unit:GetAbsOrigin()
-
-			local distance = (target_origin_difference):Length2D()
 			
 			-- Get the radian of the origin difference between the last target and the unit. We use this to figure out at what angle the unit is at relative to the the target.
 			local target_origin_difference_radian = math.atan2(target_origin_difference.y, target_origin_difference.x)
@@ -55,30 +45,37 @@ function CheckAngles(keys)
 			victim_angle = victim_angle + 180.0
 	
 			-- The difference between the world angle of the caster-target vector and the target-unit vector
-			local angle_difference = math.abs(victim_angle - ability.attacker_angle)			
+			local angle_difference = math.abs(victim_angle - attacker_angle)			
 			
-			-- Ensures the angle difference is less than the allowed width, and is the closest (to the first unit) in that width
-			if angle_difference <= attack_spill_width and distance < closest then
-				new_target = unit
-				closest = distance
+			local new_target = false
+			
+			-- Ensures the angle difference is less than the allowed width
+			if angle_difference <= attack_spill_width then
+				local info = {
+				Target = unit,
+				Source = target,
+				Ability = ability,
+				EffectName = keys.particle,
+				bDodgeable = false,
+				iMoveSpeed = 9000,
+				iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_HITLOCATION
+				}
+				ProjectileManager:CreateTrackingProjectile( info )
+				
+				new_target = true
 			end
 		end
 	end
-	
-	-- If a new target exists, we throws an attack projectile at it (and this function runs again on every new target)
-	if new_target ~= nil then
-		local info = {
-		Target = new_target,
-		Source = target,
-		Ability = ability,
-		EffectName = keys.particle,
-		bDodgeable = false,
-		iMoveSpeed = 900,
-		iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_HITLOCATION
-		}
-		ProjectileManager:CreateTrackingProjectile( info )
-	else
-	  -- Resets our attacker_angle if there are no new targets
-		ability.attacker_angle = nil
-	end
+end
+
+--[[Author: YOLOSPAGHETTI
+	Date: April 8, 2016
+	Deals damage to the secondary targets]]
+function DealDamage(keys)
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+
+	-- Applies the damage to the attack target
+	ApplyDamage({victim = target, attacker = caster, damage = ability.first_target_damage, damage_type = ability:GetAbilityDamageType()})
 end
