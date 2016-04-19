b5971e91716d98b8cee4c3139025136bf3564d08
Kyler Conena <kylerownsx62@aim.com>
2016-04-19 18:02:11 -0400

    No longer deals 0 damage every interval

--- game/scripts/vscripts/heroes/hero_bloodseeker/rupture.lua
+++ game/scripts/vscripts/heroes/hero_bloodseeker/rupture.lua
@@ -7,15 +7,16 @@ function DistanceCheck(keys)
 	local ability = keys.ability
 	local movement_damage_pct = ability:GetLevelSpecialValueFor( "movement_damage_pct", ability:GetLevel() - 1 )/100
 	local damage_cap_amount = ability:GetLevelSpecialValueFor( "damage_cap_amount", ability:GetLevel() - 1 )
-	local damage = 0
 	local position = target:GetAbsOrigin()
 	
 	if ability.origin ~= null then
 		local distance = math.sqrt((ability.origin.x - position.x)^2 + (ability.origin.y - position.y)^2)
-		if distance <= damage_cap_amount then
+		if distance <= damage_cap_amount and distance > 0 then
 			damage = distance * movement_damage_pct
 		end
 	end
 	ability.origin = position
-	ApplyDamage({victim = target, attacker = caster, damage = damage, damage_type = ability:GetAbilityDamageType()})
+	if damage ~= nil then
+		ApplyDamage({victim = target, attacker = caster, damage = damage, damage_type = ability:GetAbilityDamageType()})
+	end
 end
