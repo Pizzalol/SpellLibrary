cc96f4250ad6427e413339a4ce538c4e1cbdc11a
vVv-AA <gemina.patel@gmail.com>
2016-07-21 17:39:49 +0530

    Update necromastery.lua

--- game/scripts/vscripts/heroes/hero_nevermore/necromastery.lua
+++ game/scripts/vscripts/heroes/hero_nevermore/necromastery.lua
@@ -26,7 +26,8 @@ function NecromasteryStack( event )
 	local caster = event.caster
 	local target = event.target -- unit? The killed thing
 	local modifier = event.modifier
-
+	local ability = event.ability
+	
 	local souls_hero_bonus = ability:GetLevelSpecialValueFor( "necromastery_souls_hero_bonus", ability:GetLevel() - 1 )
 	local max_souls = ability:GetLevelSpecialValueFor( "necromastery_max_souls", ability:GetLevel() - 1 )
 
