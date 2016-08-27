c20287866ba560ce564b44c6dc0f70063181d2f9
nightborn <daniel.falkedal@icloud.com>
2016-08-27 21:44:14 +0200

    Update reality
    Now the ability works as the real one (swapping position instead of hiding the illusion)

--- game/scripts/vscripts/heroes/hero_spectre/reality.lua
+++ game/scripts/vscripts/heroes/hero_spectre/reality.lua
@@ -9,27 +9,17 @@ function RealityCast (keys)
 		
 		local target = Entities:FindByNameNearest(unit, vPoint, 0)
 
-		target.vPosition = target:GetAbsOrigin()					--Store the target illusions current position
-
 		if target:IsIllusion() then
 
+			--Store the caster and the illusions current position
+			caster.currentPosition = caster:GetAbsOrigin()
+			target.currentPosition = target:GetAbsOrigin()
+
+			--Swaps the position of the caster and the illusion
+			target:SetAbsOrigin(caster.currentPosition)	
+			caster:SetAbsOrigin(target.currentPosition)
+
 			EmitSoundOn("Hero_Spectre.Reality", caster)
-			
-			local illusions = Entities:FindAllByName(unit)
-			for i, illusion in ipairs(illusions) do
-				local illusion = illusions[i]
-				if illusion:IsIllusion() then
-					illusion:RemoveModifierByName("modifier_spectre_haunt_reality")
-					--illusion:SetAbsOrigin(target.vPosition)
-
-				end
-			end
-
-			ability:ApplyDataDrivenModifier(caster, target, "modifier_spectre_haunt_reality", {})
-
-			caster:SetAbsOrigin(target:GetAbsOrigin())		--Move caster to the illusion
-
-			target:SetAbsOrigin( target:GetAbsOrigin() - Vector(0, 0, 900) )				--Hide the illusion underground
 
 		end
 
