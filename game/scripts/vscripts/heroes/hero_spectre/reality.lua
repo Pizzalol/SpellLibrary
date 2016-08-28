db50d41b0bd161f559b22b233de4d33b877a9626
nightborn <daniel.falkedal@icloud.com>
2016-08-28 10:27:04 +0200

    Update reality.lua
    Add swapping of forward vector for caster and the target illusion (works as the original spell)

--- game/scripts/vscripts/heroes/hero_spectre/reality.lua
+++ game/scripts/vscripts/heroes/hero_spectre/reality.lua
@@ -10,6 +10,14 @@ function RealityCast (keys)
 		local target = Entities:FindByNameNearest(unit, vPoint, 0)
 
 		if target:IsIllusion() then
+			
+			--Store the caster and the illusions forward vector
+			local caster_forward_vector = caster:GetForwardVector()
+			local target_forward_vector = target:GetForwardVector()
+
+			--Swaps the forward vector of the caster and the illusion
+			caster:SetForwardVector(target_forward_vector)
+			target:SetForwardVector(caster_forward_vector)
 
 			--Store the caster and the illusions current position
 			caster.currentPosition = caster:GetAbsOrigin()
