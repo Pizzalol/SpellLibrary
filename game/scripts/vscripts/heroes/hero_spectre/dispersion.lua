0539c4e753f7317c604b973e5e2f2569b45bd39f
nightborn <daniel.falkedal@icloud.com>
2016-09-01 20:50:43 +0200

    Created dispersion.lua

---
+++ game/scripts/vscripts/heroes/hero_spectre/dispersion.lua
@@ -0,0 +1,11 @@
+LinkLuaModifier("modifier_spectre_dispersion_lua", "heroes/hero_spectre/modifiers/modifier_spectre_dispersion_lua", LUA_MODIFIER_MOTION_NONE )
+
+function ApplyModifier (keys)
+
+	local caster = keys.caster
+	local ability = keys.ability
+	local modifier = "modifier_spectre_dispersion_lua"
+
+	caster:AddNewModifier( caster, ability, modifier, {} )
+
+end
