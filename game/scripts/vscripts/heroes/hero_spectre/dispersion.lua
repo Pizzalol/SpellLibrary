66d29828587f7864008968f84fc452556c774bd2
nightborn <daniel.falkedal@icloud.com>
2016-09-01 20:57:39 +0200

    Update dispersion.lua

--- game/scripts/vscripts/heroes/hero_spectre/dispersion.lua
+++ game/scripts/vscripts/heroes/hero_spectre/dispersion.lua
@@ -1,3 +1,8 @@
+
+--[[Author: Nightborn
+	Date: August 27, 2016
+]]
+
 LinkLuaModifier("modifier_spectre_dispersion_lua", "heroes/hero_spectre/modifiers/modifier_spectre_dispersion_lua", LUA_MODIFIER_MOTION_NONE )
 
 function ApplyModifier (keys)
