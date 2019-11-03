4bd2453bc13d12af51fb21aaed9e756ffe1b89ac
MakingPanorama <50879571+MakingPanorama@users.noreply.github.com>
2019-11-03 16:05:30 +0200

    Changed Structure After Patch 7.20
    Changed Structure After Patch 7.20
    Optional

--- game/scripts/vscripts/heroes/hero_bounty_hunter/jinada.lua
+++ game/scripts/vscripts/heroes/hero_bounty_hunter/jinada.lua
@@ -1,18 +1,34 @@
 --[[Jinada
-	Author: Pizzalol
-	Date: 1.1.2015.]]
-function Jinada( keys )
+	Author: Pizzalol/DDSuper
+	Date: 11.03.2019.]]
+function JinadaStart(keys)
+
 	local ability = keys.ability
 	local level = ability:GetLevel() - 1
 	local cooldown = ability:GetCooldown(level)
-	local caster = keys.caster	
-	local modifierName = "modifier_jinada_datadriven"
+	local caster = keys.caster
+	local modifier = keys.modifier
 
-	ability:StartCooldown(cooldown)
+	local midas_particle = ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN_FOLLOW, keys.target)  -- Эффекты, частиц мидаса
+	if keys.target:IsRealHero() then
+		ability:StartCooldown(cooldown)
+		keys.target:ModifyGold(keys.StealGold, false, 0)
+		caster:ModifyGold(keys.GetGoldSelf, true, 0)
+		caster:RemoveModifierByName(modifier)
+		ParticleManager:SetParticleControlEnt(midas_particle, 1, keys.caster, PATTACH_POINT_FOLLOW, "attach_hitloc", keys.caster:GetAbsOrigin(), false) -- Контроль как и сказано
+		keys.target:EmitSound("Hero_BountyHunter.Jinata")
+		keys.target:EmitSound("DOTA_Item.Hand_Of_Midas")
+	end
 
-	caster:RemoveModifierByName(modifierName) 
-
+	if keys.target:IsCreep() then
+		caster:RemoveModifierByName(modifier)
+		keys.target:EmitSound("Hero_BountyHunter.Jinada")
+		ability:StartCooldown(cooldown)
+		ParticleManager:DestroyParticle(midas_particle, true)
+	end 
 	Timers:CreateTimer(cooldown, function()
-		ability:ApplyDataDrivenModifier(caster, caster, modifierName, {})
-		end)	
+		ability:ApplyDataDrivenModifier(caster, caster, modifier, {})
+		end)
 end
+
+
